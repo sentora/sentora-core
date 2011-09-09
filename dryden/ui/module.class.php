4d000cca75dae665427374b2ebb0fe086d622165
Bobby Allen <bobbyallen.uk@gmail.com>
2011-09-09 23:15:20 +0100

    Added a test module file to check that requested modules are being handled by the controller correctly which they now are.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- dryden/ui/module.class.php
+++ dryden/ui/module.class.php
@@ -7,37 +7,35 @@
  * @copyright ZPanel Project (http://www.zpanelcp.com/)
  * @link http://www.zpanelcp.com/
  * @license GPL (http://www.gnu.org/licenses/gpl.html)
-*/
-
+ */
 class ui_module {
     /**
      * ui_module class is used to load in the module contents into the controller.
      */
-    
+
     /**
      * Lets declare some variables here!
      */
-    
-    
     function __construct() {
         /**
          * Lets grab the controller properties and then we can use the values to set the class variables which we will then
          * use to build the UI.
          */
-
     }
-    
-    static function getModule($module){
+
+    static function getModule($module) {
         /**
          * This is where it outputs the module code to the view.
          * @var $module (string) is the folder path to the requested module.
          */
-        
-        echo "Loading module: " .$module. "";
+        if (file_exists("modules/" . $module. "/module.zpm")) {
+            $retval = file_get_contents("modules/" . $module. "/module.zpm");
+        } else {
+            $retval = "Unable to find requested module!";
+        }
+        return $retval;
     }
-    
-    
-    
+
 }
 
 ?>
