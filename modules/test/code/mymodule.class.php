7e5f36c9457db2255ce064a94bd4cd960ca21b56
Bobby Allen <bobbyallen.uk@gmail.com>
2011-09-10 14:05:15 +0100

    Added more examples to the default module showing how both module classes and core classes can be called within a module.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- modules/test/code/mymodule.class.php
+++ modules/test/code/mymodule.class.php
@@ -3,8 +3,8 @@
 class mymodule {
 
     static function EchoTest() {
-        echo "This is a new way in which ZPanel code is executed from within modules!";
-        return;
+        $string =  "This is a new way in which ZPanel code is executed from within modules!<br><br>Here is a random number for you... <strong>" .rand(0,1000). "</strong><br><br>";
+        return strtoupper($string);
     }
 
 }
