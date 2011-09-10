b2b914e06139fa844dc7fc82fdf337dbf6fc32c0
Russell Skinner <kempo19b@yahoo.com>
2011-09-10 17:50:16 -0500

    Converted phpinfo module as a test
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

---
+++ modules/phpinfo/code/mod_phpinfo.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *
+ * ZPanel - A Cross-Platform Open-Source Web Hosting Control panel.
+ * 
+ * @package ZPanel
+ * @version $Id$
+ * @author Bobby Allen - ballen@zpanelcp.com
+ * @copyright (c) 2008-2011 ZPanel Group - http://www.zpanelcp.com/
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License v3
+ *
+ * This program (ZPanel) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
+class mod_phpinfo {
+
+    static function phpinfo_show() {
+	ob_start();
+	phpinfo(INFO_GENERAL);
+	$info = ob_get_contents();
+	ob_end_clean();
+	$info = preg_replace('%^.*<body>(.*)</body>.*$%ms', '$1', $info);
+	$info = str_replace('<img border="0"', '<img style="display: none;"', $info);
+	return $info;
+    }
+
+}
+
+?>
