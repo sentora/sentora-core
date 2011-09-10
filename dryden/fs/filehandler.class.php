4790eb076bea1bd808903f781e6bf5e42dbafd4d
Russell Skinner <kempo19b@yahoo.com>
2011-09-10 16:34:22 -0500

    Added CreateFile mode to fs:fhandler
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

--- dryden/fs/filehandler.class.php
+++ dryden/fs/filehandler.class.php
@@ -3,16 +3,38 @@
 class fs_filehandler {
 
     /**
-    * Create blank or preformatted file with permissions.
+    * Clear all text in a file.
     * @author RusTus (rustus@zpanel.co.uk) 
     * @version 10.0.0
 	*/
-	static function ResetFile($value) {
+	static function ResetFile($file) {
 		$new_file = "";
-		if (is_dir($value)){
-			$fp = fopen($value,'w');
+		if (is_dir($file)){
+			$fp = fopen($file,'w');
 			fwrite($fp, $new_file);
 			fclose($fp);
+		}
+	}
+
+    /**
+    * Create blank or populated with permissions.
+    * @author RusTus (rustus@zpanel.co.uk) 
+    * @version 10.0.0
+	*/
+	static function CreateFile($path, $chmod="0777", $string="") {
+		if (!is_file($path)){
+			preg_match('`^(.+)/([a-zA-Z0-9]+\.[a-z]+)$`i', $path, $matches);
+ 			$directory = $matches[1];
+  			$file = $matches[2];
+  			if (!is_dir($directory)){
+    			if (!mkdir($directory, $chmod, 1)){
+    				return FALSE;
+    			}
+  			}
+			$fp = fopen($path,'w');
+			fwrite($fp, $string);
+			fclose($fp);
+			chmod($path, $chmod);
 		}
 	}
 	
