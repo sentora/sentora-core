31b3d87edcb5cdc463201b651ec83757cd5f53d2
Russell Skinner <kempo19b@yahoo.com>
2011-09-12 01:08:08 -0500

    Added ui_sysmessage class
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

---
+++ dryden/ui/sysmessage.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package zpanelx
+ * @subpackage dryden -> ui
+ * @author Bobby Allen (ballen@zpanelcp.com)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class ui_sysmessage {
+
+    /**
+     * ui_sysmessage class is used to send preformatted html messages that can be styled in css.
+	 * class and id default to zannounce, but can be changed as well.
+     */
+
+    static function shout($message, $class="zannounce", $id="zannounce") {
+	$line = "<div class=\"".$class."\" id=\"".$id."\">".$message."</div>";
+	echo $line;
+    }
+}
+
+?>
