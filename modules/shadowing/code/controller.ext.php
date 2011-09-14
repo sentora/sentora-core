2c3219325bc2e14a094bad2c0e8b98b2fa8ce3d5
Russell Skinner <kempo19b@yahoo.com>
2011-09-14 13:54:41 -0500

    Updated ShadowUser module
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

--- modules/shadowing/code/controller.ext.php
+++ modules/shadowing/code/controller.ext.php
@@ -44,7 +44,7 @@ class module_controller {
 		
 				while ($rowclients = $sql->fetch()) {
 					$clientuserid = ctrl_users::GetUserDetail($rowclients['ac_id_pk']);
-					$line = "<tr><td>".$clientuserid['username']."</td><td>TODO</td><td>TODO</td><td>TODO</td><td><input type=\"submit\" name=\"inShadow_".$rowclients['ac_id_pk']."\" id=\"inShadow_".$rowclients['ac_id_pk']."\" value=\"Shadow\"></td></tr>\n"; 
+					$line .= "<tr><td>".$clientuserid['username']."</td><td>TODO</td><td>TODO</td><td>TODO</td><td><input type=\"submit\" name=\"inShadow_".$rowclients['ac_id_pk']."\" id=\"inShadow_".$rowclients['ac_id_pk']."\" value=\"Shadow\"></td></tr>\n"; 
 				}
 			}else{
 			$line = "<tr><td colspan=\"5\">You have no Clients at this time.</td></tr>\n";
@@ -71,7 +71,7 @@ class module_controller {
 					while ($rowclients = $sql->fetch()) {
 					
 						if ($controller->GetControllerRequest('FORM', 'inShadow_'.$rowclients['ac_id_pk'])) {
-							 self::$shout
+							 self::$shout = "";
                              ctrl_auth::SetUserSession($rowclients['ac_id_pk']);
                              header("loaction: /");
                              exit;
