969f9bf894612a06ae2b968bbd317ac701512dcb
Bobby Allen <bobbyallen.uk@gmail.com>
2011-09-16 20:04:45 +0100

    Fully tested and fixed the new way in which 'hooks' will work in ZPX. The new method involves not using functions/methods and will execute include files in order.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- dryden/runtime/hook.class.php
+++ dryden/runtime/hook.class.php
@@ -11,17 +11,10 @@
 class runtime_hook {
 
     static function Execute($name) {
-        $mod_folder = "modules/*/code/{hook.ext.php}";
+        $mod_folder = "modules/*/hooks/{" .$name. ".hook.php}";
         foreach (glob($mod_folder, GLOB_BRACE) as $hook_file) {
-            // Check that the file exists and the class/method exists before running it!
             if (file_exists($hook_file)) {
-                $class_file = include($hook_file);
-                if (method_exists('module_hooks', 'hook' . $name . '')) {
-                    if (call_user_func(array('module_hooks', 'hook' . $name . ''))) {
-                        echo "Executed hook$name in file: $hook_file";
-                    }
-                }
-                unset($class_file);
+                include $hook_file;
             }
         }
     }
---
+++ modules/test/hooks/Test.hook.php
@@ -0,0 +1,3 @@
+<?php
+echo "This hook executed fine!";
+?>
