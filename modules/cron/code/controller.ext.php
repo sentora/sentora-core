dcf6de96540e4d03f9e15b19fcb62ca377d46785
Russell Skinner <kempo19b@yahoo.com>
2011-12-21 19:44:38 -0600

    Finnished Cron Manager module
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

--- modules/cron/code/controller.ext.php
+++ modules/cron/code/controller.ext.php
@@ -29,6 +29,7 @@ class module_controller {
 
 	static $error;
 	static $noexists;
+	static $alreadyexists;
 	static $blank;
 	static $ok;
 
@@ -47,23 +48,22 @@ class module_controller {
     			$line  = "<form action=\"./?module=cron&action=DeleteCron\" method=\"post\">";
         		$line .= "<table class=\"zgrid\">";
             	$line .= "<tr>";
-                $line .= "<th>Script</th>";
-                $line .= "<th>Description</th>";
+                $line .= "<th>".ui_language::translate("Script")."</th>";
+                $line .= "<th>".ui_language::translate("Description")."</th>";
                 $line .= "<th></th>";
             	$line .= "</tr>";
             	while ($rowcrons = $sql->fetch()) {
                 	$line .= "<tr>";
                 	$line .= "<td>" . $rowcrons['ct_script_vc'] . "</td>";
                 	$line .= "<td>" . $rowcrons['ct_description_tx'] . "</td>";
-                	$line .= "<td><input type=\"submit\" name=\"inDelete_" . $rowcrons['ct_id_pk'] . "\" id=\"inDelete_" . $rowcrons['ct_id_pk'] . "\" value=\"lang['84']\" /></td>";
+                	$line .= "<td><button class=\"fg-button ui-state-default ui-corner-all\" type=\"submit\" name=\"inDelete_" . $rowcrons['ct_id_pk'] . "\" id=\"button\" value=\"inDelete_" . $rowcrons['ct_id_pk'] . "\">".ui_language::translate("Delete")."</button></td>";
                 	$line .= "</tr>";
              	}
         		$line .= "</table>";
-        		$line .= "<input type=\"hidden\" name=\"inReturn\" value=\"GetFullURL\" /><input type=\"hidden\" name=\"inAction\" value=\"delete\" />";
     			$line .= "</form>";
 
 				} else {
-    			$line = "<p>You currently do no have any tasks setup.</p>";
+    			$line = "<h2>".ui_language::translate("You currently do no have any tasks setup.")."</h2>";
 				}
 				return $line;
 			}
@@ -79,32 +79,32 @@ class module_controller {
 		$line .= "<form action=\"./?module=cron&action=CreateCron\" method=\"post\">";
     	$line .= "<table class=\"zform\">";
         $line .= "<tr>";
-        $line .= "<th>Script:</th>";
-        $line .= "<td><input name=\"inScript\" type=\"text\" id=\"inScript\" size=\"50\" /><br />example: /folder/task.php</td>";
+        $line .= "<th>".ui_language::translate("Script").":</th>";
+        $line .= "<td><input name=\"inScript\" type=\"text\" id=\"inScript\" size=\"50\" /><br />".ui_language::translate("example").": /folder/task.php</td>";
         $line .= "</tr>";
         $line .= "<tr>";
-        $line .= "<th>Comment:</th>";
+        $line .= "<th>".ui_language::translate("Comment").":</th>";
         $line .= "<td><input name=\"inDescription\" type=\"text\" id=\"inDescription\" size=\"50\" maxlength=\"50\" /></td>";
         $line .= "</tr>";
         $line .= "<tr>";
-        $line .= "<th>Executed:</th>";
+        $line .= "<th>".ui_language::translate("Executed").":</th>";
         $line .= "<td><select name=\"inTiming\" id=\"inTiming\">";
-        $line .= "<option value=\"* * * * *\">Every 1 minute</option>";
-        $line .= "<option value=\"0,5,10,15,20,25,30,35,40,45,50,55 * * * *\">Every 5 minutes</option>";
-		$line .= "<option value=\"0,10,20,30,40,50 * * * *\">Every 10 minutes</option>";
-		$line .= "<option value=\"0,30 * * * *\">Every 30 minutes</option>";
-		$line .= "<option value=\"0 * * * *\">Every 1 hour</option>";
-		$line .= "<option value=\"0 0,2,4,6,8,10,12,14,16,18,20,22 * * *\">Every 2 hours</option>";
-		$line .= "<option value=\"0 0,8,16 * * *\">Every 8 hours</option>";
-		$line .= "<option value=\"0 0,12 * * *\">Every 12 hours</option>";
-		$line .= "<option value=\"0 0 * * *\">Every 1 day</option>";
-		$line .= "<option value=\"0 0 * * 0\">Every week</option>";
+        $line .= "<option value=\"* * * * *\">".ui_language::translate("Every 1 minute")."</option>";
+        $line .= "<option value=\"0,5,10,15,20,25,30,35,40,45,50,55 * * * *\">".ui_language::translate("Every 5 minutes")."</option>";
+		$line .= "<option value=\"0,10,20,30,40,50 * * * *\">".ui_language::translate("Every 10 minutes")."</option>";
+		$line .= "<option value=\"0,30 * * * *\">".ui_language::translate("Every 30 minutes")."</option>";
+		$line .= "<option value=\"0 * * * *\">".ui_language::translate("Every 1 hour")."</option>";
+		$line .= "<option value=\"0 0,2,4,6,8,10,12,14,16,18,20,22 * * *\">".ui_language::translate("Every 2 hours")."</option>";
+		$line .= "<option value=\"0 0,8,16 * * *\">".ui_language::translate("Every 8 hours")."</option>";
+		$line .= "<option value=\"0 0,12 * * *\">".ui_language::translate("Every 12 hours")."</option>";
+		$line .= "<option value=\"0 0 * * *\">".ui_language::translate("Every 1 day")."</option>";
+		$line .= "<option value=\"0 0 * * 0\">".ui_language::translate("Every week")."</option>";
 		$line .= "</select></td>";
 		$line .= "</tr>";
 		$line .= "<tr>";
 		$line .= "<th colspan=\"2\" align=\"right\"><input type=\"hidden\" name=\"inReturn\" value=\"GetFullURL\" />";
 		$line .= "<input type=\"hidden\" name=\"inUserID\" value=\"".$currentuser['userid']."\" />";
-		$line .= "<input type=\"submit\" name=\"inSubmit\" id=\"inSubmit\" value=\"Create\" /></th>";
+		$line .= "<button class=\"fg-button ui-state-default ui-corner-all\" type=\"submit\" id=\"button\">".ui_language::translate("Create")."</button></th>";
 		$line .= "</tr>";
 		$line .= "</table>";
 		$line .= "</form>";
@@ -117,26 +117,48 @@ class module_controller {
 		global $controller;
 		if (fs_director::CheckForEmptyValue(self::CheckCronForErrors())){
 		
-    	# If the user submitted a 'new' request then we will simply add the cron task to the database...
-	    $sql = $zdbh->prepare("INSERT INTO x_cronjobs (ct_acc_fk,
-										ct_script_vc,
-										ct_description_tx,
-										ct_created_ts) VALUES (
-										" . $controller->GetControllerRequest('FORM', 'inUserID') . ",
-										'" . $controller->GetControllerRequest('FORM', 'inScript') . "',
-										'" . $controller->GetControllerRequest('FORM', 'inDescription') . "',
-										" . time() . ")");
-		$sql->execute();
-		self::$ok = 1;
+    		# If the user submitted a 'new' request then we will simply add the cron task to the database...
+	    	$sql = $zdbh->prepare("INSERT INTO x_cronjobs (ct_acc_fk,
+											ct_script_vc,
+											ct_description_tx,
+											ct_created_ts) VALUES (
+											" . $controller->GetControllerRequest('FORM', 'inUserID') . ",
+											'" . $controller->GetControllerRequest('FORM', 'inScript') . "',
+											'" . $controller->GetControllerRequest('FORM', 'inDescription') . "',
+											" . time() . ")");
+			$sql->execute();
+			self::$ok = TRUE;
+			return;
 		}
-		
+		self::$error = TRUE;
+		return;
 	}
 	
 	static function doDeleteCron(){
-	
+		global $zdbh;
+		global $controller;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = "SELECT COUNT(*) FROM x_cronjobs WHERE ct_acc_fk=" . $currentuser['userid'] . " AND ct_deleted_ts IS NULL";
+			if ($numrows = $zdbh->query($sql)) {
+ 				if ($numrows->fetchColumn() <> 0) {	
+					$sql = $zdbh->prepare("SELECT * FROM x_cronjobs WHERE ct_acc_fk=" . $currentuser['userid'] . " AND ct_deleted_ts IS NULL");
+					$sql->execute();
+					while ($rowcrons = $sql->fetch()) {
+						if (!fs_director::CheckForEmptyValue($controller->GetControllerRequest('FORM', 'inDelete_'.$rowcrons['ct_id_pk'].''))){
+        					$sql2 = $zdbh->prepare("UPDATE x_cronjobs SET ct_deleted_ts=" . time() . " WHERE ct_id_pk=" . $rowcrons['ct_id_pk'] . "");
+							$sql2->execute();
+							self::$ok = TRUE;
+							return;
+						}
+					}
+				}
+			}
+		self::$error = TRUE;
+		return;
 	}
 	
 	static function CheckCronForErrors() {
+		global $zdbh;
 		global $controller;
 		$retval = FALSE;
 		$currentuser = ctrl_users::GetUserDetail();
@@ -150,29 +172,41 @@ class module_controller {
 			self::$noexists = TRUE;
 			$retval = TRUE;
 	    }
+	    # Check to make sure the cron is not a duplicate...
+			$sql = "SELECT COUNT(*) FROM x_cronjobs WHERE ct_acc_fk=" . $currentuser['userid'] . " AND ct_script_vc='".$controller->GetControllerRequest('FORM', 'inScript')."' AND ct_deleted_ts IS NULL";
+			if ($numrows = $zdbh->query($sql)) {
+ 				if ($numrows->fetchColumn() <> 0) {	
+					self::$alreadyexists = TRUE;
+					$retval = TRUE;
+				}
+			}
+
 		return $retval;
    	}
 	
 	static function getResult() {
 		if (!fs_director::CheckForEmptyValue(self::$blank)){
-			return ui_sysmessage::shout("You need to specify a valid location for your script.");
+			return ui_sysmessage::shout(ui_language::translate("You need to specify a valid location for your script."));
 		}
 		if (!fs_director::CheckForEmptyValue(self::$noexists)){
-			return ui_sysmessage::shout("Your script does not appear to exist at that location. Your root folder is: ");
+			return ui_sysmessage::shout(ui_language::translate("Your script does not appear to exist at that location. Your root folder is: "));
+		}
+		if (!fs_director::CheckForEmptyValue(self::$alreadyexists)){
+			return ui_sysmessage::shout(ui_language::translate("You can not add the same cron task more than once."));
 		}
 		if (!fs_director::CheckForEmptyValue(self::$error)){
-			return ui_sysmessage::shout("There was an error updating the cron job.");
+			return ui_sysmessage::shout(ui_language::translate("There was an error updating the cron job."));
 		}
 		if (!fs_director::CheckForEmptyValue(self::$ok)){
-			return ui_sysmessage::shout("Cron updated successfully.");
+			return ui_sysmessage::shout(ui_language::translate("Cron updated successfully."));
 		}else{
-			return ui_module::GetModuleDescription();
+			return ui_language::translate(ui_module::GetModuleDescription());
 		}
         return;
     }
 
 	static function getModuleName() {
-		$module_name = ui_module::GetModuleName();
+		$module_name = ui_language::translate(ui_module::GetModuleName());
         return $module_name;
     }
 
