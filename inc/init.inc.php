6087e3d21978f523b25ef1ab3090207e2618904c
Bobby Allen <bobbyallen.uk@gmail.com>
2012-01-07 12:49:58 +0000

    'Remember Me' login function is now fully working the way it should be. It will now automatically login users if they previously specified to remember their password.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -12,26 +12,32 @@ global $controller;
 $controller = new runtime_controller();
 
 if ($zlo->hasInfo()) {
-
     $zlo->writeLog();
     $zlo->reset();
 }
 
-if ((isset($_POST['inUsername'])) && (!isset($_SESSION['zpuid']))) {
-    if (!ctrl_auth::Authenticate($_POST['inUsername'], $_POST['inPassword'])) {
-        runtime_hook::Execute('OnFailedUserLogin');
-        ctrl_auth::RequireUser();
+if (isset($_POST['inUsername'])) {
+    if (!isset($_POST['inRemember'])) {
+        $rememberdetails = false;
     } else {
-        runtime_hook::Execute('OnUserLogin');
+        $rememberdetails = true;
     }
-} else {
+    ctrl_auth::Authenticate($_POST['inUsername'], md5($_POST['inPassword']), $rememberdetails, false);
+}
+
+if (isset($_COOKIE['zUser'])) {
+    ctrl_auth::Authenticate($_COOKIE['zUser'], $_COOKIE['zPass'], false, true);
+}
+
+if (!isset($_SESSION['zpuid'])) {
     ctrl_auth::RequireUser();
 }
 
 if (isset($_GET['logout'])) {
     ctrl_auth::KillSession();
+    ctrl_auth::KillCookies();
     runtime_hook::Execute('OnUserLogout');
-    include 'etc/styles/zpanelx/login.ztml';
+    header("location: ./?loggedout");
     exit;
 }
 
