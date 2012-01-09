1db541fbbca70a6788c25a79e04cafa3b8ec32e3
Bobby Allen <bobbyallen.uk@gmail.com>
2012-01-09 09:08:16 +0000

    Adding PHPMailer library into our /etc/lib/ folder which I will use in our new ws_email class to enable the ability for ZPanel users to configure authenticated external SMTP servers to send email from.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

---
+++ dryden/sys/email.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Email class used for sending out emails from ZPanel.
+ * This class extends on the PHPMailer library included in etc/lib/PHPMailer!
+ *
+ * @package zpanelx
+ * @subpackage dryden -> sys
+ * @version 1.0.0
+ * @author ballen (ballen@zpanelcp.com)
+ */
+
+require './etc/lib/PHPMailer/class.phpmailer.php';
+
+class ws_email extends PHPMailer  {
+    
+    
+    
+}
+
+?>
--- dryden/sys/monitoring.class.php
+++ dryden/sys/monitoring.class.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Class provides server port monitoring and uptime reporting functionality.
+ *
+ * @package zpanelx
+ * @subpackage dryden -> sys
+ * @version 1.0.0
+ * @author ballen (ballen@zpanelcp.com)
+ */
+
 class sys_monitoring {
 
     /**
