56a3f4fb4a0bb1911f806bf95816e879a08b8ea2
Russell Skinner <kempo19b@yahoo.com>
2012-01-10 21:44:33 -0600

    Upadated DNS Manager
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

--- modules/dns_manager/code/controller.ext.php
+++ modules/dns_manager/code/controller.ext.php
@@ -106,6 +106,223 @@ class module_controller {
 		}		
 
 	}
+
+	static function getA_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='A' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">\n";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>\n";
+			$line .= "<div class=\"TTL\">\n";
+			$line .= "<input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\">\n";
+			$line .= "<input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\">\n";
+			$line .= "</div>\n";
+			$line .= "<div class=\"in\">IN</div>\n";
+			$line .= "<div class=\"type\">A</div>\n";
+			$line .= "<div class=\"target\">\n";
+			$line .= "<input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\">\n";
+			$line .= "<input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\">\n";
+			$line .= "</div>\n";
+			$line .= "<span class=\"delete enableToolTip\"></span>\n";
+			$line .= "<span class=\"undo enableToolTip\"></span>\n";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"A\" type=\"hidden\">\n";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">\n";
+			$line .= "<br>\n";
+			$line .= "</div>\n";
+		}		
+		return $line;
+	}
+
+	static function getAAAA_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='AAAA' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">\n";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>\n";
+			$line .= "<div class=\"TTL\">\n";
+			$line .= "<input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\">\n";
+			$line .= "<input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\">\n";
+			$line .= "</div>\n";
+			$line .= "<div class=\"in\">IN</div>\n";
+			$line .= "<div class=\"type\">AAAA</div>\n";
+			$line .= "<div class=\"target\">\n";
+			$line .= "<input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\">\n";
+			$line .= "<input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\">\n";
+			$line .= "</div>\n";
+			$line .= "<span class=\"delete enableToolTip\"></span>\n";
+			$line .= "<span class=\"undo enableToolTip\"></span>\n";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"AAAA\" type=\"hidden\">\n";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">\n";
+			$line .= "<br>\n";
+			$line .= "</div>\n";
+		}		
+		return $line;
+	}
+
+	static function getCNAME_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='CNAME' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\">";
+			$line .= "<input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\">";
+			$line .= "<input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">CNAME</div>";
+			$line .= "<div class=\"target\">";
+			$line .= "<input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\">";
+			$line .= "<input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\">";
+			$line .= "</div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span>";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"CNAME\" type=\"hidden\">";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+
+	static function getMX_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='MX' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\"><input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\"><input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">MX</div>";
+			$line .= "<div class=\"priority\"><input name=\"priority[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_priority_in']."\" type=\"text\"><input name=\"original_priority[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_priority_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"target\"><input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\"><input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\"></div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span><input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"MX\" type=\"hidden\"><input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+
+	static function getTXT_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='TXT' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\"><input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\"><input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">TXT</div>";
+			$line .= "<div class=\"target\"><input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_texttarget_tx']."\" type=\"text\"><input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_texttarget_tx']."\" type=\"hidden\"></div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span><input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"TXT\" type=\"hidden\"><input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+	
+	static function getSRV_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='SRV' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\"><input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\"><input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">SRV</div>";
+			$line .= "<div class=\"priority\"><input name=\"priority[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_priority_in']."\" type=\"text\"><input name=\"original_priority[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_priority_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"weight\"><input name=\"weight[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_weight_in']."\" type=\"text\"><input name=\"original_weight[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_weight_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"port\"><input name=\"port[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_port_in']."\" type=\"text\"><input name=\"original_port[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_port_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"target\"><input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\"><input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\"></div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span>";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"SRV\" type=\"hidden\">";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+
+	static function getSPF_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='SPF' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\"><input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\"><input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">SPF</div>";
+			$line .= "<div class=\"target\"><input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\"><input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\"></div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span>";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"SPF\" type=\"hidden\">";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+	
+	static function getNS_Records(){
+		global $zdbh;
+		global $controller;
+		$line = NULL;
+		$currentuser = ctrl_users::GetUserDetail();
+		$sql = $zdbh->prepare("SELECT * FROM x_dns WHERE dn_acc_fk=" . $currentuser['userid'] . " AND dn_type_vc='NS' AND dn_deleted_ts IS NULL ORDER BY dn_host_vc ASC");
+		$sql->execute();
+		while ($rowdns = $sql->fetch()) {
+			$line .= "<div class=\"dnsRecord row\">";
+			$line .= "<div class=\"hostName\"><span>".$rowdns['dn_host_vc']."</span></div>";
+			$line .= "<div class=\"TTL\"><input name=\"ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"text\"><input name=\"original_ttl[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_ttl_in']."\" type=\"hidden\"></div>";
+			$line .= "<div class=\"in\">IN</div>";
+			$line .= "<div class=\"type\">NS</div>";
+			$line .= "<div class=\"target\"><input name=\"target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"text\"><input name=\"original_target[".$rowdns['dn_id_pk']."]\" value=\"".$rowdns['dn_target_vc']."\" type=\"hidden\"></div>";
+			$line .= "<span class=\"delete enableToolTip\"></span>";
+			$line .= "<span class=\"undo enableToolTip\"></span>";
+			$line .= "<input name=\"type[".$rowdns['dn_id_pk']."]\" value=\"NS\" type=\"hidden\">";
+			$line .= "<input class=\"delete\" name=\"delete[".$rowdns['dn_id_pk']."]\" value=\"false\" type=\"hidden\">";
+			$line .= "<br>";
+			$line .= "</div>";
+		}		
+		return $line;
+	}
+
+	static function doSaveDNS(){
+		global $zdbh;
+		global $controller;
+		$line = print_r($_POST);
+		//self::$editdomain = $controller->GetControllerRequest('FORM', 'inDomain');
+		return $line;
+	}
 	
 	static function doDisplayRecords(){
 		global $zdbh;
