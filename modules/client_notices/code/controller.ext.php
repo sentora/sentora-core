60df92a825b020d9f082a3862e63e75c7c93f6ab
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-12 17:28:27 +0000

    Adding new client notice module. Need rustus to provide an icon for the module to match the current X theme.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

---
+++ modules/client_notices/code/controller.ext.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ *
+ * ZPanel - A Cross-Platform Open-Source Web Hosting Control panel.
+ * 
+ * @package ZPanel
+ * @version $Id$
+ * @author Bobby Allen - ballen@zpanelcp.com
+ * @copyright (c) 2008-2011 ZPanel Group - http://www.zpanelcp.com/
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License v3
+ *
+ * This program (ZPanel) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+class module_controller {
+
+    static function getModuleName() {
+        $module_name = ui_language::translate(ui_module::GetModuleName());
+        return $module_name;
+    }
+
+    static function getModuleIcon() {
+        global $controller;
+        $module_icon = "modules/" . $controller->GetControllerRequest('URL', 'module') . "/assets/icon.png";
+        return $module_icon;
+    }
+
+    /**
+     * The 'worker' methods.
+     */
+    static function ExectuteUpdateNotice($uid, $notice) {
+        global $zdbh;
+        $sql = $zdbh->prepare("
+            UPDATE x_accounts
+            SET ac_notice_tx = '" . $notice . "'
+            WHERE ac_id_pk = " . $uid . "");
+        $sql->execute();
+        return true;
+    }
+
+    static function ExecuteShowNotice($rid) {
+        global $zdbh;
+        $result = $zdbh->query("SELECT ac_notice_tx FROM x_accounts WHERE ac_id_pk = " . $rid . "")->Fetch();
+        if ($result) {
+            return $result['ac_notice_tx'];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * End 'worker' methods.
+     */
+
+    /**
+     * Webinterface sudo methods.
+     */
+    static function getCurrentNoticeText() {
+        global $controller;
+        $currentuser = ctrl_users::GetUserDetail();
+        return self::ExecuteShowNotice($currentuser['resellerid']);
+    }
+
+    static function doUpdateMessage() {
+        global $controller;
+        $currentuser = ctrl_users::GetUserDetail();
+        $formvars = $controller->GetAllControllerRequests('FORM');
+        self::ExectuteUpdateNotice($currentuser['userid'], $formvars['inNotice']);
+        header("location: ./?module=" . $controller->GetCurrentModule() . "&saved=true");
+        exit;
+    }
+
+    /**
+     * Webinterface sudo methods.
+     */
+}
+?>
