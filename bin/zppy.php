0d7e71b2081886a5df215e062da67ce196be1981
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-13 18:36:36 +0000

    More progress on the zppy client, adding command line arguments and general CLI stuff. Shebang line added too!
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- bin/zppy.php
+++ bin/zppy.php
@@ -1,5 +1,5 @@
+#!/usr/bin/php
 <?php
-
 /**
  * ZPPY - The ZPanel Package Manager Tool.
  * @package zpanelx
@@ -23,25 +23,39 @@ if (!runtime_controller::IsCLI())
 
 if ((isset($argv[1])) && ($argv[1] == 'install')) {
     echo "Module to be installed..\n";
+
+    exit;
+}
+if ((isset($argv[1])) && ($argv[1] == 'upgrade')) {
+    echo "Module to be upgraded!\n";
+
+    exit;
 }
 if ((isset($argv[1])) && ($argv[1] == 'remove')) {
     echo "Module to be removed!\n";
+
+    exit;
 }
-if (($argv[1] == 'version') || ($argv[1] == '-v') || ($argv[1] == '--version')) {
+if ((isset($argv[1])) && (($argv[1] == 'version') || ($argv[1] == '-v') || ($argv[1] == '--version'))) {
     echo ctrl_options::GetOption('dbversion') . "\n";
+
+    exit;
 }
 if ((!isset($argv[1])) || ($argv[1] == 'help') || ($argv[1] == '-h') || ($argv[1] == '--help')) {
-    echo "ZPanel Package Manager\n";
+    echo "\nZPanel Package Manager\n";
     echo "Copyright (c) 2008 - 2012 ZPanel Project\n";
     echo "http://www.zpanelcp.com/\n";
-    echo "\n";
     echo "Usage: zppy [action] [modulename]\n";
     echo "Actions:\n";
     echo "  install [modulename]- Install a named module.\n";
-    echo "  supgrade [modulename]- Updates a named module.\n";
+    echo "  upgrade [modulename]- Updates a named module.\n";
     echo "  remove [modulename]- Remove a named module.\n";
     echo "  --version - Displays the Zpanel version\n";
-    echo "  --help - Displays this text.\n";
+    echo "  --help - Displays this text.\n\n";
+}
+
+if (isset($argv[1])) {
+    echo "Unknown action \"" . $argv[1] . "\" check syntax with: zppy --help\n";
 }
 exit;
 ?>
