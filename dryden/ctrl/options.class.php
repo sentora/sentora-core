9726425e265b54cf7565b0c344720888d59fcffc
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-14 18:49:06 +0000

    Implemented a new feature which allows per system setting predefined options to be created. If there are some predefined options it will instead output a drop-down menu with those options.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- dryden/ctrl/options.class.php
+++ dryden/ctrl/options.class.php
@@ -12,7 +12,7 @@ class ctrl_options {
 
     /**
      * The main 'getter' class used to retrieve the value from the system options table.
-     * 
+     * @author Bobby Allen (ballen@zpanelcp.com)
      * @global array $zdbh
      * @param string $name
      * @return array 
@@ -29,7 +29,7 @@ class ctrl_options {
 
     /**
      * The main 'setter' class used to write/update system options.
-     * 
+     * @author Bobby Allen (ballen@zpanelcp.com)
      * @global array $zdbh
      * @param string $name
      * @param string $value
@@ -55,7 +55,7 @@ class ctrl_options {
 
     /**
      * Gets user account information.
-     * 
+     * @author Bobby Allen (ballen@zpanelcp.com)
      * @global array $zdbh
      * @param string $name
      * @return array 
@@ -72,7 +72,7 @@ class ctrl_options {
 
     /**
      * Gets user package information.
-     * 
+     * @author Bobby Allen (ballen@zpanelcp.com)
      * @global array $zdbh
      * @param string $name
      * @return array 
@@ -81,7 +81,7 @@ class ctrl_options {
         global $zdbh;
         $result = $zdbh->query("SELECT * FROM x_accounts WHERE ac_id_pk = '$id'")->Fetch();
         if ($result) {
-            $packageid = $result['ac_id_pk']; //change this to package id
+            $packageid = $result['ac_id_pk'];
             $result = $zdbh->query("SELECT * FROM x_packages WHERE pk_id_pk = '$packageid'")->Fetch();
             if ($result) {
                 return $result;
@@ -91,6 +91,60 @@ class ctrl_options {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Checks for predefined avaliable options to choose from.
+     * @author Bobby Allen (ballen@zpanelcp.com)
+     * @param type $dval The container to hold the predefined values (seperated with a '|' pipe charater!)
+     * @return boolean 
+     */
+    public static function CheckForPredefinedOptions($dval) {
+        if ($dval == "")
+            return false;
+        return true;
+    }
+
+    /**
+     * Dynamically builds a standard text field and will populate with a value if one is set.
+     * @author Bobby Allen (ballen@zpanelcp.com)
+     * @param type $name
+     * @param type $cval
+     * @return string The HTML code for the generated text field.
+     */
+    public static function OutputSettingTextField($name, $cval = "") {
+        if ($cval == "")
+            return "<input type=\"text\" name=\"" . $name . "\" id=\"" . $name . "\">\n";
+        return "<input type=\"text\" name=\"" . $name . "\" id=\"" . $name . "\" value=\"" . $cval . "\">\n";
+    }
+
+    /**
+     * Dynamicaly builds a single line text area and will populate with the value if one is set.
+     */
+    public static function OuputSettingTextArea($name, $cval = "") {
+        return "<textarea cols=\"30\" rows=\"1\" name=\"" . $name . "\">" . $cval . "</textarea>";
+    }
+
+    /**
+     * Dynamically builds a drop-down menu of avaliable options based on predfined list of values (seperated with a '|' pipe character!)
+     * @author Bobby Allen (ballen@zpanelcp.com)
+     * @param type $name The name of the text field and 'id' to use.
+     * @param type $dval The predefined list of avaliable options.
+     * @param type $cval The current value of the field (optional) - Will autoamtically select this if one is set.
+     * @return string The HTML code for the generated drop down.
+     */
+    public static function OuputSettingMenuField($name, $dval, $cval = "") {
+        $values = explode("|", $dval);
+        $field = "<select name=\"" . $name . "\" id=\"" . $name . "\">\n";
+        foreach ($values as $option) {
+            if ($cval != $option) {
+                $field .= "\t<option value=\"" . $option . "\">" . $option . "</option>\n";
+            } else {
+                $field .= "\t<option value=\"" . $option . "\" selected=\"selected\">" . $option . "</option>\n";
+            }
+        }
+        $field .= "</select>\n";
+        return $field;
     }
 
 }
