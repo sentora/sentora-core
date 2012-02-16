e50114ea74ff685e6a9e2a95f08900bfdfa9f043
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-16 23:46:13 +0000

    New template tag added to automatically get and use the correct CSS file if more than one and the reseller has chosen a different one to that of the default (default.css) - All working perfect. Just need to update the 'Theme Manager' now to enable user choice of CSS if more than one is found.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- dryden/ctrl/users.class.php
+++ dryden/ctrl/users.class.php
@@ -34,6 +34,7 @@ class ctrl_users {
         $userdetail->addItemValue('packageid', $dbvals['ac_package_fk']);
         $userdetail->addItemValue('enabled', $dbvals['ac_enabled_in']);
         $userdetail->addItemValue('usertheme', $dbvals['ac_usertheme_vc']);
+        $userdetail->addItemValue('usercss', $dbvals['ac_usercss_vc']);
         $userdetail->addItemValue('fullname', $dbvals['ud_fullname_vc']);
         $userdetail->addItemValue('packagename', $dbvals['pk_name_vc']);
         $userdetail->addItemValue('usergroup', $dbvals['ug_name_vc']);
@@ -53,58 +54,12 @@ class ctrl_users {
         $userdetail->addItemValue('forwardersquota', $dbvals['qt_fowarders_in']);
         $userdetail->addItemValue('distrobutionlistsquota', $dbvals['qt_distlists_in']);
 
-
-        /*
-          # Display the current logged in your details!
-          $rows = $zdbh->prepare("SELECT * FROM x_accounts LEFT JOIN x_profiles ON (x_accounts.ac_id_pk=x_profiles.ud_user_fk) LEFT JOIN x_groups ON (x_profiles.ud_group_fk=x_groups.ug_id_pk) LEFT JOIN x_packages ON (x_profiles.ud_package_fk=x_packages.pk_id_pk) LEFT JOIN x_quotas ON (x_profiles.ud_package_fk=x_quotas.qt_package_fk) WHERE x_accounts.ac_id_pk = " . ctrl_auth::CurrentUserID() . "");
-          $rows->execute();
-          $dbvals = $rows->fetch();
-          $userdetail->addItemValue('username', $dbvals['ac_user_vc']);
-          $userdetail->addItemValue('userid', $dbvals['ac_id_pk']);
-          $userdetail->addItemValue('password', $dbvals['ac_pass_vc']);
-          $userdetail->addItemValue('email', $dbvals['ac_email_vc']);
-          $userdetail->addItemValue('fullname', $dbvals['ud_fullname_vc']);
-          $userdetail->addItemValue('packagename', $dbvals['pk_name_vc']);
-          $userdetail->addItemValue('usergroup', $dbvals['ug_name_vc']);
-          $userdetail->addItemValue('usergroupid', $dbvals['ac_group_fk']);
-          $userdetail->addItemValue('address', $dbvals['ud_address_tx']);
-          $userdetail->addItemValue('postcode', $dbvals['ud_postcode_vc']);
-          $userdetail->addItemValue('phone', $dbvals['ud_phone_vc']);
-          $userdetail->addItemValue('language', $dbvals['ud_language_vc']);
-          $userdetail->addItemValue('diskquota', $dbvals['qt_diskspace_bi']);
-          $userdetail->addItemValue('bandwidthquota', $dbvals['qt_bandwidth_bi']);
-          $userdetail->addItemValue('domainquota', $dbvals['qt_domains_in']);
-          $userdetail->addItemValue('subdomainquota', $dbvals['qt_subdomains_in']);
-          $userdetail->addItemValue('parkeddomainquota', $dbvals['qt_parkeddomains_in']);
-          $userdetail->addItemValue('ftpaccountsquota', $dbvals['qt_ftpaccounts_in']);
-          $userdetail->addItemValue('mysqlquota', $dbvals['qt_mysql_in']);
-          $userdetail->addItemValue('mailboxquota', $dbvals['qt_mailboxes_in']);
-          $userdetail->addItemValue('forwardersquota', $dbvals['qt_fowarders_in']);
-          $userdetail->addItemValue('distrobutionlistsquota', $dbvals['qt_distlists_in']);
-          } else {
-          $rows = $zdbh->query("SELECT * FROM x_accounts JOIN x_profiles ON x_accounts.ac_id_pk=x_profiles.ud_user_fk WHERE x_accounts.ac_id_pk = " . $user . "");
-          $rows->execute();
-          $dbvals = $rows->fetch();
-          $userdetail->addItemValue('username', $dbvals['ac_user_vc']);
-          $userdetail->addItemValue('userid', $dbvals['ac_id_pk']);
-          $userdetail->addItemValue('password', $dbvals['ac_pass_vc']);
-          $userdetail->addItemValue('email', $dbvals['ac_email_vc']);
-          $userdetail->addItemValue('fullname', $dbvals['ud_fullname_vc']);
-          $userdetail->addItemValue('packagename', $dbvals['pk_name_vc']);
-          $userdetail->addItemValue('usergroup', $dbvals['ug_name_vc']);
-          $userdetail->addItemValue('usergroupid', $dbvals['ac_group_fk']);
-          $userdetail->addItemValue('address', $dbvals['ud_address_tx']);
-          $userdetail->addItemValue('postcode', $dbvals['ud_postcode_vc']);
-          $userdetail->addItemValue('phone', $dbvals['ud_phone_vc']);
-          $userdetail->addItemValue('language', $dbvals['ud_language_vc']);
-
-          }
-         */
         return $userdetail->getDataObject();
     }
 
-    #Get all active domains on user account
-
+    /**
+     * @todo Does this still need to be here as this is now managed under a module and not seen as 'core' but template still relies on this at present! - Bobby Allen
+     */
     static function GetUserDomains($userid, $type = "1") {
         global $zdbh;
         $domains = 0;
@@ -118,6 +73,13 @@ class ctrl_users {
         return $domains;
     }
 
+    /**
+     * Checks that the specified user (from their UID) is active and therefore allowed to login to the panel.
+     * @author Bobby Allen (ballen@zpanelcp.com)
+     * @global type $zdbh
+     * @param type $userid
+     * @return type 
+     */
     static function CheckUserEnabled($userid) {
         global $zdbh;
         $domains = 0;
---
+++ dryden/ui/tpl/csspath.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package zpanelx
+ * @subpackage dryden -> ui -> tpl
+ * @author Bobby Allen (ballen@zpanelcp.com)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class ui_tpl_csspath {
+
+    public function Template() {
+        $user = ctrl_users::GetUserDetail();
+        if (!fs_director::CheckForEmptyValue(fs_director::CheckForEmptyValue($user['usercss']))) {
+            $retval = "etc/styles/" . ui_template::GetUserTemplate() . "/css/default.css";
+        } else {
+            $retval = "etc/styles/" . ui_template::GetUserTemplate() . "/css/" . $user['usercss'] . ".css";
+        }
+        return $retval;
+    }
+
+}
+
+?>
