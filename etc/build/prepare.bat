7d91d55b286376d678667e7468d01c8981c72fdf
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-19 14:13:15 +0000

    Adding Windows folder preparation script (creates the default ZPanel paths etc. ready for installation.)
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

---
+++ etc/build/prepare.bat
@@ -0,0 +1,33 @@
+@echo off
+cls
+rem ZPanel Enviroment Configuration Tool for Microsoft Windows based systems.
+rem Written by Bobby Allen, 19/02/2012
+
+echo ZPanel Enviroment Configuration Tool
+echo ====================================
+echo.
+echo We recommend you install ZPanel on drive 'C' otherwise you will have to make
+echo manual changes to our pre-configured application configuration files for
+echo Apache, Filezilla etc. as well as update all paths in the x_settings table
+echo in the ZPanelX database.
+echo.
+echo If you need help, please visit our forums: http://forums.zpanelcp.com/
+echo.
+set /p zpd=What drive would you like to install ZPanel on? (eg: C): %=%
+IF [%zpd%]==[] SET zpd=C
+echo.
+echo Creating folder structure...
+mkdir %zpd%:\zpanel
+mkdir %zpd%:\zpanel\panel
+mkdir %zpd%:\zpanel\bin
+mkdir %zpd%:\zpanel\configs
+mkdir %zpd%:\zpanel\hostdata
+mkdir %zpd%:\zpanel\logs
+mkdir %zpd%:\zpanel\backups
+mkdir %zpd%:\zpanel\temp
+echo Complete!
+echo.
+echo You can access your new ZPanel folder layout here: %zpd%:\zpanel
+echo.
+pause
+exit
