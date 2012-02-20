cd77ec77796c1cbf22470694972f74e0f43eee9a
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-20 11:19:58 +0000

    Updated the sys/archive.class.php file to include the function to unzip a file using the PHP ZIP functionality.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- dryden/sys/archive.class.php
+++ dryden/sys/archive.class.php
@@ -11,9 +11,26 @@
  * @license GPL (http://www.gnu.org/licenses/gpl.html)
  */
 class sys_archive {
-    
-    
-    
+
+    /**
+     * Uncompresses a ZIP archive to a given location.
+     * @author Bobby Allen (ballen@zpanelcp.com)
+     * @param string $archive Full path and filename to the ZIP archive.
+     * @param string $dest The full path to the folder to extract the archive into (with trailing slash!)
+     * @return boolean 
+     */
+    static function Unzip($archive, $dest) {
+        $zip = new ZipArchive;
+        $result = $zip->open($archive);
+        if ($result == true) {
+            $zip->extractTo($dest);
+            $zip->close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 ?>
