03c95faee419665f4b33a58fda45866db33b4063
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-20 11:10:34 +0000

    Adding new sys_archive class to handle zipping and uncompressing of compressed archives.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

---
+++ dryden/sys/archive.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Interface template selection class.
+ * @package zpanelx
+ * @subpackage dryden -> ui
+ * @version 1.0.0
+ * @author Bobby Allen (ballen@zpanelcp.com)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class sys_archive {
+    
+    
+    
+}
+
+?>
