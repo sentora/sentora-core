c84d8be5adf786ac4e5478e3e89b9446db505718
Bobby Allen <bobbyallen.uk@gmail.com>
2012-02-24 21:18:04 +0000

    Added a batch file to work as a pass through script to enable easy use of the 'zppy' CLI client from the Windows command line, this is working perfectly. Also updated the 'prepare.bat' file to install the CLI alias script (batch file) into the Windows folder to 'auto-register' the PATH variable and instantly make the 'zppy' command callable from the windows command prompt in the same way that Linux behaves etc.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

---
+++ etc/build/bin/zppy.bat
@@ -0,0 +1,4 @@
+REM Provides a command line pass through for the ZPanel 'zppy' client.
+REM This file should be placed in the Windows\System folder to enable easy use of the 'zppy' command.
+@echo off
+php C:\zpanel\panel\bin\zppy %1
