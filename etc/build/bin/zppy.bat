8a11c284d591be7e83a44eda57cedf87f8935825
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-01 09:32:15 +0000

    Fixed issue with bug #21 (http://bugs.zpanelcp.com/view.php?id=21) issue identified as a regression bug, issue was caused by the batch file only passing the first command line argument to the PHP script, fixed this by adding the maximum argument passes to the batch file (x3) - Have tested and is working great now. - Only a bug when using the new bat file way of easily adding the zppy CLI extension to the PATH environmental variable list.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- etc/build/bin/zppy.bat
+++ etc/build/bin/zppy.bat
@@ -1,5 +1,4 @@
 @echo off
 REM Provides a command line pass through for the ZPanel 'zppy' client.
-
 REM This file should be placed in the Windows\System folder to enable easy use of the 'zppy' command.
-php C:\zpanel\panel\bin\zppy %1
+php C:\xampp\htdocs\zpanelx\bin\zppy %1 %2 %3
