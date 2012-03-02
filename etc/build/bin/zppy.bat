2238810df0b5e1cee7d8a6c97ed5c1ae63c74953
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-02 20:40:47 +0000

    Fixed zppy.bat file where it was previously pointing to my local development path. (http://bugs.zpanelcp.com/view.php?id=35)
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- etc/build/bin/zppy.bat
+++ etc/build/bin/zppy.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Provides a command line pass through for the ZPanel 'zppy' client.
 REM This file should be placed in the Windows\System folder to enable easy use of the 'zppy' command.
-php C:\xampp\htdocs\zpanelx\bin\zppy %1 %2 %3
+php C:\zpanel\panel\bin\zppy %1 %2 %3
