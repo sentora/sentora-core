b270a1c1f728ef1bdbccdaf21dd1ea415fa804b3
Russell Skinner <kempo19b@yahoo.com>
2012-03-06 23:33:32 -0600

    Fixed Perl Versions for windows also put limit on 4 domains to show up on domain info sidebar, with a link to "show all"
    Signed-off-by: Russell Skinner <kempo19b@yahoo.com>

--- dryden/ui/tpl/clientdomains.class.php
+++ dryden/ui/tpl/clientdomains.class.php
@@ -17,11 +17,12 @@ class ui_tpl_clientdomains {
         $currentuser = ctrl_users::GetUserDetail();			
 		$line = "<table  border=\"0\" cellSpacing=\"0\" cellPadding=\"2\" width=\"100%\">";
 
-        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=1 AND vh_deleted_ts IS NULL ORDER BY vh_name_vc";
+        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=1 AND vh_deleted_ts IS NULL ORDER BY vh_id_pk LIMIT 4";
         $numrows = $zdbh->query($sql);
         if ($numrows->fetchColumn() <> 0) {
             $sql = $zdbh->prepare($sql);
             $sql->execute();
+			$limit = 0;
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Domains</strong></td><td></td></tr>";
             while ($rowdomains = $sql->fetch()) {
 				if ($rowdomains['vh_type_in'] == 1){
@@ -40,19 +41,23 @@ class ui_tpl_clientdomains {
 						$line .= "<a href=\"#\" title=\"Disabled\"><img src=\"<# ui_tpl_assetfolderpath #>/images/disabled.png\"></a> ";
 					}
 				}
+				$limit++;
 			}
-			//$line .= "<tr><td>&nbsp;</td><td></td></tr>";
+			if ($limit >= 4){
+				$line .= "<tr><td>&nbsp;&nbsp;&nbsp;<a href=\"?module=domains\">(Show All)</a></td><td></td></tr>";
+			}
 		} else {
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Domains</strong></td><td></td></tr>";
 			$line .= "<tr><td nowrap=\"nowrap\" style=\"text-indent:14px;\"><span class=\"Side_Info_None\">No Domains Found</span></td><td><a href=\"?module=domains\">CREATE</a></td></tr>";
 			//$line .= "<tr><td>&nbsp;</td><td></td></tr>";
 		}
 
-        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=2 AND vh_deleted_ts IS NULL ORDER BY vh_name_vc";
+        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=2 AND vh_deleted_ts IS NULL ORDER BY vh_id_pk LIMIT 4";
         $numrows = $zdbh->query($sql);
         if ($numrows->fetchColumn() <> 0) {
             $sql = $zdbh->prepare($sql);
             $sql->execute();
+			$limit = 0;
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Sub Domains</strong></td><td></td></tr>";
             while ($rowdomains = $sql->fetch()) {
 				if ($rowdomains['vh_type_in'] == 2){
@@ -71,19 +76,23 @@ class ui_tpl_clientdomains {
 						$line .= "<a href=\"#\" title=\"Disabled\"><img src=\"<# ui_tpl_assetfolderpath #>/images/disabled.png\"></a> ";
 					}
 				}
+				$limit++;
 			}
-			//$line .= "<tr><td>&nbsp;</td><td></td></tr>";
+			if ($limit >= 4){
+				$line .= "<tr><td>&nbsp;&nbsp;&nbsp;<a href=\"?module=sub_domains\">(Show All)</a></td><td></td></tr>";
+			}
 		} else {
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Sub Domains</strong></td></td><td></tr>";
 			$line .= "<tr><td nowrap=\"nowrap\" style=\"text-indent:14px;\"><span class=\"Side_Info_None\">No Sub Domains Found</span></td><td><a href=\"?module=sub_domains\">CREATE</a></td></tr>";
 			//$line .= "<tr><td>&nbsp;</td><td></td></tr>";
 		}
 		
-        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=3 AND vh_deleted_ts IS NULL ORDER BY vh_name_vc";
+        $sql = "SELECT * FROM x_vhosts WHERE vh_acc_fk=" .$currentuser['userid']. " AND vh_type_in=3 AND vh_deleted_ts IS NULL ORDER BY vh_id_pk LIMIT 4";
         $numrows = $zdbh->query($sql);
         if ($numrows->fetchColumn() <> 0) {
             $sql = $zdbh->prepare($sql);
             $sql->execute();
+			$limit = 0;
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Parked Domains</strong></td><td></td></tr>";
             while ($rowdomains = $sql->fetch()) {
 				if ($rowdomains['vh_type_in'] == 3){
@@ -102,8 +111,11 @@ class ui_tpl_clientdomains {
 						$line .= "<a href=\"#\" title=\"Disabled\"><img src=\"<# ui_tpl_assetfolderpath #>/images/disabled.png\"></a> ";
 					}
 				}
+				$limit++;
 			}
-			//$line .= "<tr><td>&nbsp;</td><td></td></tr>";
+			if ($limit >= 4){
+				$line .= "<tr><td>&nbsp;&nbsp;&nbsp;<a href=\"?module=parked_domains\">(Show All)</a></td><td></td></tr>";
+			}
 		} else {
 			$line .= "<tr><td nowrap=\"nowrap\"><img class=\"raquo\" src=\"<# ui_tpl_assetfolderpath #>/images/blank.png\" border=\"0\"><strong>Parked Domains</strong></td></td><td></tr>";
 			$line .= "<tr><td nowrap=\"nowrap\" style=\"text-indent:14px;\"><span class=\"Side_Info_None\">No Parked Domains Found</span></td><td><a href=\"?module=parked_domains\">CREATE</a></td></tr>";
