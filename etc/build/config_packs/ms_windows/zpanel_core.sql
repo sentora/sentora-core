383d23abf55e63c9ee53e2a13676090e2cbe5b23
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-10 10:40:49 +0000

    Added CREATE DATABASE statements to the SQL scripts to improve the install process.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- etc/build/config_packs/ms_windows/zpanel_core.sql
+++ etc/build/config_packs/ms_windows/zpanel_core.sql
@@ -9,6 +9,9 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE `zpanel_core` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 USE `zpanel_core`;
 
 /*Table structure for table `x_accounts` */
