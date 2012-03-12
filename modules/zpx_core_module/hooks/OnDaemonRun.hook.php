762731b83d1498c41ca79a81632220b5c1e77763
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-12 19:14:43 +0000

    Code cleanup, commented out the echo'ing of the current usage per user during daemon execution as its not needed and was just using it for debugging.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- modules/zpx_core_module/hooks/OnDaemonRun.hook.php
+++ modules/zpx_core_module/hooks/OnDaemonRun.hook.php
@@ -15,13 +15,11 @@ while ($userdir = $userssql->fetch()) {
         $size = 0;
     }
     $checksql = $zdbh->query("SELECT COUNT(*) AS total FROM x_bandwidth WHERE bd_month_in = " . date("Ym") . " AND bd_acc_fk = " . $userdir['ac_id_pk'] . "")->fetch();
-    #$checksql->execute();
     if ($checksql['total'] == 0) {
-        $insertsql = $zdbh->query("INSERT INTO x_bandwidth (bd_acc_fk, bd_month_in, bd_transamount_bi, bd_diskamount_bi) VALUES (" . $userdir['ac_id_pk'] . "," . date("Ym") . ",0,0);");
-        #$insertsql->execute();
+        $zdbh->query("INSERT INTO x_bandwidth (bd_acc_fk, bd_month_in, bd_transamount_bi, bd_diskamount_bi) VALUES (" . $userdir['ac_id_pk'] . "," . date("Ym") . ",0,0);");
     }
     $updatesql = $zdbh->query("UPDATE x_bandwidth SET bd_diskamount_bi = '" . $size . "' WHERE bd_acc_fk =" . $userdir['ac_id_pk'] . "");
     $updatesql->execute();
-    echo "Disk usage for user \"" . $userdir['ac_user_vc'] . "\" is: " . $size . "\n";
+    //echo "Disk usage for user \"" . $userdir['ac_user_vc'] . "\" is: " . $size . "\n";
 }
 ?>
