78b73a9d6d8c5823e9dd94952529a024d455de9b
Bobby Allen <bobbyallen.uk@gmail.com>
2012-03-15 17:36:59 +0000

    Fixed spelling mistake in the DB connection error.
    Signed-off-by: Bobby Allen <bobbyallen.uk@gmail.com>

--- inc/dbc.inc.php
+++ inc/dbc.inc.php
@@ -35,7 +35,7 @@ try {
                     color: #666;
             }
             </style>
-            <div class=\"dbwarning\"><strong>Critical Error:</strong> [0100] - Unable to connect or authenticate to the ZPanel database (<em>$dbname</em>).<p>We advice that you contact the server administator to ensure that the dataase server is online and that the correct connection parameter are being used.</p></div>";
+            <div class=\"dbwarning\"><strong>Critical Error:</strong> [0100] - Unable to connect or authenticate to the ZPanel database (<em>$dbname</em>).<p>We advise that you contact the server administator to ensure that the dataase server is online and that the correct connection parameter are being used.</p></div>";
     die($error_html);
 }
 ?>
