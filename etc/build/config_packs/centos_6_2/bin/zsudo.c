09ab57075b778e4aa6943068b311ddb7459a17ec
kevin <kevin@kevin-laptop>
2012-05-19 23:11:29 +0100

    non GNU licence removed

--- etc/build/config_packs/centos_6_2/bin/zsudo.c
+++ etc/build/config_packs/centos_6_2/bin/zsudo.c
@@ -1,29 +1,5 @@
 /*****************************************************************************
-* ZPanel -                                                                   *
-* Copyright (C) Bobby Allen  & The ZPanel Development team, 2009-Present     *
-* Email: ballen@zpanel.co.uk                                                 *
-* Website: http://www.zpanel.co.uk                                           *
-* -------------------------------------------------------------------------- *
-* BY USING THIS SOFTWARE/SCRIPT OR ANY FUNCTION PROVIDED IN THE SOURCE CODE  *
-* YOU AGREE THAT YOU MUST NOT DO THE FOLLOWING:-                             *
-*                                                                            *
-*     1) REMOVE THE COPYRIGHT INFOMATION                                     *
-*     2) RE-PACKAGE AND/OR RE-BRAND THIS SOFTWARE                            *
-*     3) AGREE TO THE FOLLOWING DISCLAIMER...                                *
-*                                                                            *
-* DISCLAIMER                                                                 *
-* -------------------------------------------------------------------------- *
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS        *
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  *
-* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR *
-* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR           *
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,      *
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        *
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;*
-* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR    *
-* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF     *
-* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                 *
+* ZPanel                                                                     *                           *
 *----------------------------------------------------------------------------*
 * COMPILE NOTES                                                              *
 * make sure to chmod +s the binary after compiling:                          *
