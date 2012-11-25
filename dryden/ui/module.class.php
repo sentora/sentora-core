9f240a23761268a064e69076c383d0827abe2cbf
mottley <smottley@zpanelcp.com>
2012-11-25 15:33:49 +0000

    New code now works (Tested quickly by lottfy) sticking with the new code for now

--- dryden/ui/module.class.php
+++ dryden/ui/module.class.php
@@ -153,21 +153,7 @@ class ui_module {
      */
     static function CheckModuleEnabled($modulename) {
         global $zdbh;
-        /**
-         * Sam Mottley:- Seem to be an error in daemon saying module disabled just checking this function temp code change!
-            $bindArray = array(
-                ':module' => $modulename,
-            );
-            $retvalSQL = $zdbh->bindQuery("SELECT mo_name_vc, mo_enabled_en FROM x_modules WHERE mo_folder_vc = :module", $bindArray);
-            $retval = $zdbh->returnRow();
-
-            if ($retval['mo_enabled_en'] == "true") {
-                $retval = true;
-            } else {
-                $retval = false;
-            }
-            return $retvalEnd;
-        */
+        
         $numrows = $zdbh->prepare("SELECT mo_name_vc, mo_enabled_en FROM x_modules WHERE mo_name_vc = :module");
         $numrows->bindParam(':module', $modulename);
         $numrows->execute();
