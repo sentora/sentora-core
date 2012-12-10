ec66ae113c5632d3ed2660512d813866972ce825
Bobby Allen <bobbyallen.uk@gmail.com>
2012-12-10 09:50:46 +0000

    Fixes a few issues that Lottfy has reported after testing the new setzadmin utility. Also added alias batch file for Windows users.

---
+++ etc/build/bin/setzadmin.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Provides a command line pass through for the ZPanel 'setzadmin' client.
+REM This file should be placed in the Windows\System folder to enable easy use of the 'setzadmin' command.
+php C:\zpanel\panel\bin\setzadmin %1 %2 %3
