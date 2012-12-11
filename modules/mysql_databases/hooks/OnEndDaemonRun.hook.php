d2683c85b448517b18f97c3678af3b6b834c328a
mottley <smottley@zpanelcp.com>
2012-12-11 22:05:09 +0000

    all sqli in this module have been fixed

--- modules/mysql_databases/hooks/OnEndDaemonRun.hook.php
+++ modules/mysql_databases/hooks/OnEndDaemonRun.hook.php
@@ -15,13 +15,23 @@ function AddCurrentDatabaseSize() {
     $usersql = $zdbh->query("SELECT ac_id_pk,ac_user_vc FROM x_accounts WHERE ac_deleted_ts IS NULL");
     while ($users = $usersql->fetch()) {
         $totalforuser = 0;
-        $userdbssql = $zdbh->query("SELECT my_name_vc, my_usedspace_bi FROM x_mysql_databases WHERE my_acc_fk = " . $users['ac_id_pk'] . " AND my_deleted_ts IS NULL");
+        //$userdbssql = $zdbh->query("SELECT my_name_vc, my_usedspace_bi FROM x_mysql_databases WHERE my_acc_fk = " . $users['ac_id_pk'] . " AND my_deleted_ts IS NULL");       
+        $userdbssql = $zdbh->prepare("SELECT my_name_vc, my_usedspace_bi FROM x_mysql_databases WHERE my_acc_fk = :ac_id_pk AND my_deleted_ts IS NULL");
+        $userdbssql->bindParam(':ac_id_pk', $users['ac_id_pk']);
+        $userdbssql->execute();
+        
         while ($userdbs = $userdbssql->fetch()) {
             $totalforuser = $totalforuser + $userdbs['my_usedspace_bi'];
             // echo "User: " . $users['ac_user_vc'] . " Database: " . $userdbs['my_name_vc'] . " Size: " . $userdbs['my_usedspace_bi'] . "\n";
         }
         // echo "Total DB storage size: " . $totalforuser . " for user " . $users['ac_user_vc'] . "\n";
-        $zdbh->query("UPDATE x_bandwidth SET bd_diskamount_bi = (bd_diskamount_bi+" . $totalforuser . ") WHERE bd_acc_fk = " . $users['ac_id_pk'] . " AND bd_month_in = " . date('Ym') . "");
+        //$zdbh->query("UPDATE x_bandwidth SET bd_diskamount_bi = (bd_diskamount_bi+" . $totalforuser . ") WHERE bd_acc_fk = " . $users['ac_id_pk'] . " AND bd_month_in = " . date('Ym') . "");
+        $date = date('Ym');
+        $numrows = $zdbh->prepare("UPDATE x_bandwidth SET bd_diskamount_bi = (bd_diskamount_bi+:totalforuser) WHERE bd_acc_fk = :ac_id_pk AND bd_month_in = :date");
+        $numrows->bindParam(':totalforuser', $totalforuser);
+        $numrows->bindParam(':date', $date);
+        $numrows->bindParam(':ac_id_pk', $users['ac_id_pk']);
+        $numrows->execute();
     }
 }
 
