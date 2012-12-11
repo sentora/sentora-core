3fa4b4a387ee0d7187def6a06314d94cd54b6059
Kevin Andrews <kandrews@zpanelcp.com>
2012-12-11 00:39:44 +0000

    Corrected stats chmod permission Modified command to run webalizer by changing directory first then executing the command, works fine in centos and ubuntu cli & httpd

--- modules/webalizer_stats/hooks/OnDaemonHour.hook.php
+++ modules/webalizer_stats/hooks/OnDaemonHour.hook.php
@@ -20,13 +20,13 @@ function GenerateWebalizerStats() {
     echo "Generating webalizer stats html..." . fs_filehandler::NewLine();
     while ($rowvhost = $sql->fetch()) {
         if (!file_exists(ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'])) {
-            @mkdir(ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'], 777, TRUE);
+            @mkdir(ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'], 0755, TRUE);
         }
         if (sys_versions::ShowOSPlatformVersion() == "Windows") {
             $runcommand = ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/bin/webalizer.exe -o " . ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . " -d -F clf -n " . $rowvhost['vh_name_vc'] . "  " . ctrl_options::GetSystemOption('log_dir') . "domains/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . "-access.log";
         } else {
             chmod(ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/bin/webalizer", 4777);
-            $runcommand = ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/bin/webalizer -o " . ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . " -d -F clf -n " . $rowvhost['vh_name_vc'] . "  " . ctrl_options::GetSystemOption('log_dir') . "domains/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . "-access.log";
+            $runcommand =  "(cd " . ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/bin" . " && exec " . "webalizer -o " . ctrl_options::GetSystemOption('zpanel_root') . "modules/webalizer_stats/stats/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . " -d -F clf -n " . $rowvhost['vh_name_vc'] . "  " . ctrl_options::GetSystemOption('log_dir') . "domains/" . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . "-access.log)";
         }
         echo "Generating stats for: " . $rowvhost['ac_user_vc'] . "/" . $rowvhost['vh_name_vc'] . fs_filehandler::NewLine();
         system($runcommand);
