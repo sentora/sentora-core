63d060df2efc3309bb8fd3b9e12b1633afe4d4e7
mottley <smottley@zpanelcp.com>
2012-12-11 22:04:54 +0000

    all sqli in this module have been fixed

--- modules/mysql_databases/hooks/OnAfterDeleteClient.hook.php
+++ modules/mysql_databases/hooks/OnAfterDeleteClient.hook.php
@@ -10,19 +10,30 @@ function DeleteClientDatabases() {
         $sql = $zdbh->prepare($sql);
         $sql->execute();
         while ($rowclient = $sql->fetch()) {
-            $rowdatabase = $zdbh->query("SELECT * FROM x_mysql_databases WHERE my_acc_fk=" . $rowclient['ac_id_pk'] . " AND my_deleted_ts IS NULL")->fetch();
+            //$rowdatabase = $zdbh->query("SELECT * FROM x_mysql_databases WHERE my_acc_fk=" . $rowclient['ac_id_pk'] . " AND my_deleted_ts IS NULL")->fetch();
+            
+            $numrows = $zdbh->prepare("SELECT * FROM x_mysql_databases WHERE my_acc_fk=:ac_id_pk AND my_deleted_ts IS NULL");
+            $numrows->bindParam(':ac_id_pk', $rowclient['ac_id_pk']);
+            $numrows->execute();
+            $rowdatabase = $numrows->fetch();
+        
             if ($rowdatabase) {
                 try {
-                    $delete = $zdbh->prepare("DROP DATABASE IF EXISTS `" . $rowdatabase['my_name_vc'] . "`;");
+                    $delete = $zdbh->prepare("DROP DATABASE IF EXISTS `:my_name_vc`;");
+                    $delete->bindParam(':my_name_vc', $rowdatabase['my_name_vc'], PDO::PARAM_STR);
                     $delete->execute();
                     $delete = $zdbh->prepare("FLUSH PRIVILEGES");
                     $delete->execute();
                     $delete = $zdbh->prepare("UPDATE x_mysql_databases 
-						SET my_deleted_ts = '" . time() . "' 
-						WHERE my_acc_fk = '" . $rowclient['ac_id_pk'] . "'");
+						SET my_deleted_ts = :time 
+						WHERE my_acc_fk = :ac_id_pk");
+                    $delete->bindParam(':ac_id_pk', $rowclient['ac_id_pk']);
+                    $time = time();
+                    $delete->bindParam(':time', $time);
                     $delete->execute();
                     $delete = $zdbh->prepare("DELETE FROM x_mysql_dbmap 
-						WHERE mm_database_fk=" . $rowdatabase['my_id_pk'] . "");
+						WHERE mm_database_fk=:my_id_pk");
+                    $delete->bindParam(':my_id_pk', $rowdatabase['my_id_pk']);
                     $delete->execute();
                 } catch (PDOException $e) {
                     
