a2278635243a36c4a6ee768560a8878a7c04c97e
Kevin Andrews <kandrews@zpanelcp.com>
2012-12-17 23:40:09 +0000

    Update SQL scripts for ZPanel 10.0.0 to 10.0.1 Roundcube 0.7.* to 0.8.*

---
+++ etc/build/config_packs/ms_windows/zpanelx-update/10-0-1/zpanel_update.sql
@@ -0,0 +1,14 @@
+/* Update SQL for Windows ZPanel 10.0.0 to 10.0.1 */
+
+/* Adds the new fields for customising vhost configs */
+ALTER TABLE `zpanel_core`.`x_vhosts` 
+ADD COLUMN `vh_custom_port_in` INT(6) NULL DEFAULT NULL  AFTER `vh_custom_tx` , 
+ADD COLUMN `vh_portforward_in` INT(1) NULL DEFAULT '1'  AFTER `vh_custom_port_in` , 
+ADD COLUMN `vh_custom_ip_vc` VARCHAR(45) NULL DEFAULT NULL  AFTER `vh_portforward_in` ;
+
+
+
+
+
+
+
---
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-0-1/zpanel_update.sql
@@ -0,0 +1,18 @@
+/* Update SQL for Ubuntu 12 ZPanel 10.0.0 to 10.0.1 */
+
+/* Adds the new fields for customising vhost configs */
+ALTER TABLE `zpanel_core`.`x_vhosts`
+ADD COLUMN `vh_custom_port_in` INT(6) NULL DEFAULT NULL  AFTER `vh_custom_tx` ,
+ADD COLUMN `vh_portforward_in` INT(1) NULL DEFAULT '1'  AFTER `vh_custom_port_in` ,
+ADD COLUMN `vh_custom_ip_vc` VARCHAR(45) NULL DEFAULT NULL  AFTER `vh_portforward_in` ;
+
+/* New Postfix User for new postfix configs */
+CREATE USER postfix@localhost IDENTIFIED BY 'postfix';
+GRANT ALL PRIVILEGES ON zpanel_postfix . * TO postfix@localhost;
+FLUSH PRIVILEGES;
+
+/* Change the Uid and Gid of all FTP users to enable editing of apache owned files */
+ALTER TABLE `zpanel_proftpd`.`ftpuser`
+CHANGE COLUMN `uid` `uid` SMALLINT(6) NOT NULL DEFAULT '33'  ,
+CHANGE COLUMN `gid` `gid` SMALLINT(6) NOT NULL DEFAULT '33'  ;
+UPDATE `zpanel_proftpd`.`ftpuser` SET `uid`='33', `gid`='33';
