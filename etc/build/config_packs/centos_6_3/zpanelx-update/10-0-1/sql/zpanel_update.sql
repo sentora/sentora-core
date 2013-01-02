525168a5f0470c9c6224ebf19f00994052da0142
Kevin Andrews <kandrews@zpanelcp.com>
2013-01-02 01:12:27 +0000

    Ubuntu update sql scripts updated with changes.

--- etc/build/config_packs/centos_6_3/zpanelx-update/10-0-1/sql/zpanel_update.sql
+++ etc/build/config_packs/centos_6_3/zpanelx-update/10-0-1/sql/zpanel_update.sql
@@ -1,5 +1,5 @@
 /* Update SQL for CentOS 6 ZPanel 10.0.0 to 10.0.1 */
-
+USE `zpanel_core`;
 /* New Postfix User for new postfix configs */
 GRANT ALL PRIVILEGES  ON zpanel_postfix.*
 TO 'postfix'@'localhost' IDENTIFIED BY 'postfix';
@@ -12,9 +12,6 @@ CHANGE COLUMN `uid` `uid` SMALLINT(6) NOT NULL DEFAULT '48',
 CHANGE COLUMN `gid` `gid` SMALLINT(6) NOT NULL DEFAULT '48';
 UPDATE `zpanel_proftpd`.`ftpuser` SET `uid`='48', `gid`='48';
 
-/* Force DNS and Apache rewrite configs */
-UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`=',0' WHERE `so_name_vc`='dns_hasupdates';
-UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`='true' WHERE `so_name_vc`='apache_changed';
-
+USE `zpanel_core`;
 /* Update the ZPanel database version number */
 UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  '10.0.1' WHERE  `so_name_vc` = 'dbversion';
--- etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-0-1/sql/zpanel_update.sql
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-0-1/sql/zpanel_update.sql
@@ -1,14 +1,9 @@
 /* Update SQL for Ubuntu 12 ZPanel 10.0.0 to 10.0.1 */
 USE `zpanel_core`;
-/* Adds the new fields for customising vhost configs */
-ALTER TABLE `zpanel_core`.`x_vhosts`
-ADD COLUMN `vh_custom_port_in` INT(6) NULL DEFAULT NULL  AFTER `vh_custom_tx` ,
-ADD COLUMN `vh_portforward_in` INT(1) NULL DEFAULT '1'  AFTER `vh_custom_port_in` ,
-ADD COLUMN `vh_custom_ip_vc` VARCHAR(45) NULL DEFAULT NULL  AFTER `vh_portforward_in` ;
 
 /* New Postfix User for new postfix configs */
-CREATE USER postfix@localhost IDENTIFIED BY 'postfix';
-GRANT ALL PRIVILEGES ON zpanel_postfix . * TO postfix@localhost;
+GRANT ALL PRIVILEGES  ON zpanel_postfix.*
+TO 'postfix'@'localhost' IDENTIFIED BY 'postfix';
 FLUSH PRIVILEGES;
 
 USE `zpanel_proftpd`;
@@ -18,5 +13,10 @@ CHANGE COLUMN `uid` `uid` SMALLINT(6) NOT NULL DEFAULT '33'  ,
 CHANGE COLUMN `gid` `gid` SMALLINT(6) NOT NULL DEFAULT '33'  ;
 UPDATE `zpanel_proftpd`.`ftpuser` SET `uid`='33', `gid`='33';
 
+/* Force DNS and Apache rewrite configs */
+UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`=',0' WHERE `so_name_vc`='dns_hasupdates';
+UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`='true' WHERE `so_name_vc`='apache_changed';
+
+USE `zpanel_core`;
 /* Update the ZPanel database version number */
 UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  '10.0.1' WHERE  `so_name_vc` = 'dbversion';
