8c0fce1cac4c2212771a2600e3b1583ff7935616
mottley <smottley@zpanelcp.com>
2013-02-04 19:43:38 +0000

    Bug Fixed Type Typo in the db_driver

--- modules/htpasswd/assets/js/connectors/jqueryFileTree.php
+++ modules/htpasswd/assets/js/connectors/jqueryFileTree.php
@@ -12,7 +12,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/inc/dbc.inc.php');
 $z_db_user = $user;
 $z_db_pass = $pass;
 try {
-    $zdbh = new db_driver("mysql:host=" . $host . ";dbbname=" . $dbname . "", $z_db_user, $z_db_pass);
+    $zdbh = new db_driver("mysql:host=" . $host . ";dbname=" . $dbname . "", $z_db_user, $z_db_pass);
 } catch (PDOException $e) {
     exit();
 }
@@ -24,7 +24,10 @@ $rows->execute();
 $dbvals = $rows->fetch();
 $username = $dbvals['ac_user_vc'].'/public_html/';
 
+//find zpanel settings
 $rowsettings = $zdbh->query("SELECT * FROM x_settings WHERE so_name_vc='hosted_dir'")->fetch();
+
+
 $path = str_replace('//','/',str_replace('\\','/',str_replace('\\\\','//',str_replace('../','',str_replace('./','',urldecode($_POST['dir'])))))); //this is total untrusted data so lets secure it
 
 if (file_exists($rowsettings['so_value_tx'].$username.$path)) {
@@ -49,7 +52,13 @@ if (file_exists($rowsettings['so_value_tx'].$username.$path)) {
             if (file_exists($rowsettings['so_value_tx'].$username.$path . $file) && $file != '.' && $file != '..' && !is_dir($rowsettings['so_value_tx'].$username.$path . $file) && strstr($file, '.htaccess')) {
                 $ext = preg_replace('/^.*\./', '', $file);
                 $htaccesspath = trim(substr($path, strlen($rowsettings['so_value_tx']), strlen($rowsettings['so_value_tx'].$username.$path . $file)));
-                $rowpath = $zdbh->query("SELECT * FROM x_htaccess WHERE ht_dir_vc='" . substr($htaccesspath, 0, -1) . "' AND ht_deleted_ts IS NULL")->fetch();
+                //$rowpath = $zdbh->query("SELECT * FROM x_htaccess WHERE ht_dir_vc='" . substr($htaccesspath, 0, -1) . "' AND ht_deleted_ts IS NULL")->fetch();
+                $rowpathQuery = $zdbh->prepare("SELECT * FROM x_htaccess WHERE ht_dir_vc = :htaccesspath AND ht_deleted_ts IS NULL");
+                $bindhtaccesspath = substr($htaccesspath, 0, -1);
+                $rowpathQuery->bindParam(':htaccesspath', $bindhtaccesspath);
+                $rowpathQuery->execute();
+                $rowpath = $rows->fetch();
+                
                 if ($rowpath) {
                     echo "<li class=\"file ext_$ext\"><a href=\"./?module=htpasswd&selected=Selected&show=Edit&other=" . $rowpath['ht_id_pk'] . "\" title=\"Edit Users\">" . htmlentities($file) . "</a></li>";
                 } else {
