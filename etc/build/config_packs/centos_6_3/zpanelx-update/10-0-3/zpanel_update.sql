a55f0d59cdd3a8a32d36fb344033cfb28a2ceff6
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-17 14:52:17 +0000

    Added DROP TABLE syntax to the 10.0.3 upgrade scripts for the redundent table and also upgraded each of the installer scripts so that this old table is no longer created during installation. Also fixed the code in the Usage Viewer webservice so that the number of used MySQL databases are queried from the correct 'x_mysql_databases' table instead of 'x_mysql'. This is now ready for full QA testing and will be released as part of the next release of ZPX. This fixes bug #408 (http://bugs.zpanelcp.com/view.php?id=408)

--- etc/build/config_packs/centos_6_3/zpanelx-update/10-0-3/zpanel_update.sql
+++ etc/build/config_packs/centos_6_3/zpanelx-update/10-0-3/zpanel_update.sql
@@ -6,3 +6,6 @@ UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`='/var/zpanel/logs/zpanel.log
 
 /* Update the ZPanel database version number */
 UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  '10.0.3' WHERE  `so_name_vc` = 'dbversion';
+
+/* Drop the redunent x_mysql table */
+DROP TABLE IF EXISTS `zpanel_core`.`x_mysql`;
--- etc/build/config_packs/ms_windows/zpanelx-update/10-0-3/zpanel_update.sql
+++ etc/build/config_packs/ms_windows/zpanelx-update/10-0-3/zpanel_update.sql
@@ -1,8 +1,5 @@
-/* Update SQL for Ubuntu 12.04 ZPanel 10.0.2 to 10.0.3 */
+/* Update SQL for Windows ZPanel 10.0.2 to 10.0.3 */
 USE `zpanel_core`;
-
-UPDATE `zpanel_core`.`x_settings` SET `so_value_tx`='/var/zpanel/logs/zpanel.log' WHERE `so_name_vc`='logfile';
-
 
 /* Update the ZPanel database version number */
 UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  '10.0.3' WHERE  `so_name_vc` = 'dbversion';
