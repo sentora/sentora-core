a9e126c3688eab515e53c545cb77d983119995cc
Jason Davis <jason.davis.fl@gmail.com>
2013-04-20 02:36:12 -0400

    New System message/notice class to utilize Bootstrap's amazing Alerts.  Drydens sysmessage class will handle the old class names as well as Bootstraps names and will link them all to use Bootstrap HTML and CSS

--- dryden/ui/sysmessage.class.php
+++ dryden/ui/sysmessage.class.php
@@ -13,20 +13,48 @@
 class ui_sysmessage {
 
     /**
-     * Used to send preformatted html messages that can be styled in CSS. The class and id tags for the DIV default to zannounce, but can be changed too for example 'zwarning'.
-     * Bobby Allen (ballen@zpanelcp.com)
+     * Show HTML Alert Messages
+     * Jason Davis (jason.davis.fl@gmail.com)
      * @param string $message The message to output to the screen.
      * @param string $class The CSS class name to use on the DIV.
-     * @param string $id The CSS ID name to use on the DIV.
+     * @param string $title An Optional Heading/Title Message
+     * @param string $closeBtn Optional TRUE or FALSE to show a Close button or not
+     *
      * @return string The generated HTML source code.
      */
-    static function shout($message, $class = "zannounce", $id = "zannounce") {
+    static function shout($message,  $class = "zannounce", $title = '', $closeBtn = true) {
+
+        // Convert ZPanel CSS Class to Bootstrap Class
+        switch ($class) {
+            case 'zannounce':
+            case 'zannounceinfo':
+            case 'alert-info':
+                $class = 'alert-info';
+                break;
+            case 'zannounceerror':
+            case 'alert-error':
+                $class = 'alert-error';
+                break;
+            case 'zannouncesuccess':
+            case 'alert-success':
+                $class = 'alert-success';
+                break;
+            case 'zannounceprimary':
+            case 'alert-primary':
+                $class = 'alert-primary';
+                break;
+            default:
+                $class = 'alert-info';
+        }
+
         runtime_hook::Execute('OnBeforeSysMessageShout');
-        $line = "<div class=\"" . $class . "\" id=\"" . $id . "\"><a href=\"#\" class=\"zannounce_a\" id=\"zannounce_a\"></a>" . $message . "</div>";
+        $line = '<div class="alert alert-block '. $class. '">';
+        $heading = $title ? '<h4>'.$title.'</h4>' : '';
+        $closeBtn = $closeBtn ? '<button type="button" class="close" data-dismiss="alert">×</button>' : '';
+        $line .= $closeBtn . $heading.'<p>' .$message. '</p></div>';
         runtime_hook::Execute('OnAfterSysMessageShout');
         return $line;
     }
-
 }
 
 ?>
