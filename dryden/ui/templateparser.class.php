b3451d13c5ed7c73b9447788dc7ea6cadef94075
Bobby Allen <bobbyallen.uk@gmail.com>
2013-07-09 00:36:57 +0100

    Fixes issue with PHP 5.4 code of which is not backwards compatible with our current install packages and what the mainstream version of PHP is current used by most Linux OSes, changed the code to use call_user_func() instead, resolves issue with PHP 5.3.

--- dryden/ui/templateparser.class.php
+++ dryden/ui/templateparser.class.php
@@ -10,7 +10,8 @@
  * @link http://www.zpanelcp.com/
  * @license GPL (http://www.gnu.org/licenses/gpl.html)
  */
-class ui_templateparser {
+class ui_templateparser
+{
 
     /**
      * Array of all the functions allowed by ZPanelX Template system with the pattern to identify them
@@ -26,7 +27,7 @@ class ui_templateparser {
         'EchoLoop' => "/<&\s([\w*]*)\s&>/is",
         'If' => " /<%\sif\s([\w*-*_*]*)\s%>/is",
         'Else' => "/<%\s(else)\s%>/is",
-        'EndIf' => "/<%\s(endif)\s%>/is"      
+        'EndIf' => "/<%\s(endif)\s%>/is"
     );
 
     /**
@@ -52,11 +53,12 @@ class ui_templateparser {
      * Runs though the functions array and loads the relivent function compiler
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static function CompileFunctions($data){
+    static function CompileFunctions($data)
+    {
         $temp = $data;
         runtime_hook::Execute('OnBeforeTemplateProcessor');
-        foreach(ui_templateparser::$Functions as $Tag => $pattern){
-            $temp = ui_templateparser::{"Compile{$Tag}"}($pattern, $temp);
+        foreach (ui_templateparser::$Functions as $Tag => $pattern) {
+            $temp = call_user_func_array('ui_templateparser::Compile' . $Tag, array($pattern, $temp));
         }
         runtime_hook::Execute('OnAfterTemplateProcessor');
         return $temp;
@@ -66,15 +68,16 @@ class ui_templateparser {
      * Removes any php tags
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompilePHPTags($value, $data){
+    static private function CompilePHPTags($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if (($match) && (!empty($match[1]))) {
             $i = 0;
             foreach ($match[1] as $string) {
-                if($match[0][$i] == '?>'){
+                if ($match[0][$i] == '?>') {
                     $data = str_replace($match[0][$i], ']', $data);
-                }else{
+                } else {
                     $data = str_replace($match[0][$i], 'PHP execution is not permitted! Caught: [', $data);
                 }
                 $i++;
@@ -83,35 +86,36 @@ class ui_templateparser {
         return $data;
     }
 
-
     /**
      * Compiles ZPanelX loop template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileLoop($value, $data){
+    static private function CompileLoop($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if (($match) && (!empty($match[1]))) {
             $i = 0;
             foreach ($match[1] as $function) {
-                $data = str_replace($match[0][$i], '<?php foreach(module_controller::get'.$function.'() as $key => $value){ ?>', $data);
+                $data = str_replace($match[0][$i], '<?php foreach(module_controller::get' . $function . '() as $key => $value){ ?>', $data);
                 $i++;
             }
         }
         return $data;
-    } 
+    }
 
     /**
      * Compiles ZPanelX echo loop template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileEchoLoop($value, $data){
+    static private function CompileEchoLoop($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if (($match) && (!empty($match[1]))) {
             $i = 0;
             foreach ($match[1] as $key) {
-                $data = str_replace($match[0][$i], '<?php echo $value[\''.$key.'\']; ?>', $data);
+                $data = str_replace($match[0][$i], '<?php echo $value[\'' . $key . '\']; ?>', $data);
                 $i++;
             }
         }
@@ -122,7 +126,8 @@ class ui_templateparser {
      * Compiles ZPanelX end loop template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileEndLoop($value, $data){
+    static private function CompileEndLoop($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if (($match) && (!empty($match[1]))) {
@@ -139,13 +144,14 @@ class ui_templateparser {
      * Compiles ZPanelX if template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileIf($value, $data){
+    static private function CompileIf($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if (($match) && (!empty($match[1]))) {
             $i = 0;
             foreach ($match[1] as $function) {
-                $data = str_replace($match[0][$i], '<?php if(module_controller::get'.$function.'()){ ?>', $data);
+                $data = str_replace($match[0][$i], '<?php if(module_controller::get' . $function . '()){ ?>', $data);
                 $i++;
             }
         }
@@ -156,7 +162,8 @@ class ui_templateparser {
      * Compiles ZPanelX end if template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileEndIf($value, $data){
+    static private function CompileEndIf($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if ($match) {
@@ -173,14 +180,15 @@ class ui_templateparser {
      * Compiles ZPanelX lanuage template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileLanuage($value, $data){
+    static private function CompileLanuage($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if ($match) {
             $i = 0;
             foreach ($match[1] as $string) {
                 $output = ui_language::translate(addslashes($string));
-                $data = str_replace($match[0][$i], "<?php echo ui_language::translate('".addslashes($string)."');?>", $data);
+                $data = str_replace($match[0][$i], "<?php echo ui_language::translate('" . addslashes($string) . "');?>", $data);
                 $i++;
             }
         }
@@ -191,7 +199,8 @@ class ui_templateparser {
      * Compiles ZPanelX function each template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileFunctionEcho($value, $data){
+    static private function CompileFunctionEcho($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if ($match) {
@@ -199,7 +208,7 @@ class ui_templateparser {
             foreach ($match[1] as $classes) {
                 $method_name = "get" . $classes;
                 $output = module_controller::$method_name();
-                $data = str_replace($match[0][$i], "<?php echo module_controller::".$method_name."(); ?>", $data);
+                $data = str_replace($match[0][$i], "<?php echo module_controller::" . $method_name . "(); ?>", $data);
                 $i++;
             }
         }
@@ -210,7 +219,8 @@ class ui_templateparser {
      * Compiles ZPanelX end if template tags into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileElse($value, $data){
+    static private function CompileElse($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if ($match) {
@@ -227,7 +237,8 @@ class ui_templateparser {
      * Compiles ZPanelX template class tag into valid PHP
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static private function CompileTemplateClass($value, $data){
+    static private function CompileTemplateClass($value, $data)
+    {
         $match = null;
         preg_match_all($value, $data, $match);
         if ($match) {
@@ -236,7 +247,7 @@ class ui_templateparser {
                 if (class_exists('' . $classes . '')) {
                     $moduleTemplate = call_user_func(array($classes, 'Template'));
                     $codeToInsert = ui_templateparser::CompileFunctions($moduleTemplate);
-                    $data = str_replace($match[0][$i], $codeToInsert, $data);             
+                    $data = str_replace($match[0][$i], $codeToInsert, $data);
                 }
                 $i++;
             }
@@ -248,7 +259,8 @@ class ui_templateparser {
      * Check the cache file for presents and valid/upto date Data
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static function CheckFileCache($phpCode){
+    static function CheckFileCache($phpCode)
+    {
         $currentCode = $phpCode;
 
         //Get the users current theme
@@ -256,31 +268,31 @@ class ui_templateparser {
         $userTheme = $userDetails['usertheme'];
 
         //The location of the cached php
-        if(isset($_GET['module'])){
+        if (isset($_GET['module'])) {
             $location = ui_templateparser::$storageLocation . $userTheme . '/' . md5(fs_protector::SanitiseFolderName($_GET['module'])) . '.cache';
-        }else{
+        } else {
             $location = ui_templateparser::$storageLocation . $userTheme . '/' . md5('index') . '.cache';
         }
 
         //check folder exists (First load of 10.1.0 and on new theme)
         $dirname = dirname($location);
-        if (!is_dir($dirname)){
+        if (!is_dir($dirname)) {
             mkdir($dirname, 0755, true);
         }
 
-        //check file exsists if not make, insert code else just get current code 
-        if(file_exists($location)){
+        //check file exsists if not make, insert code else just get current code
+        if (file_exists($location)) {
             $content = file_get_contents($location);
-        }else{
+        } else {
             $handle = fopen($location, 'w');
             file_put_contents($location, $phpCode, LOCK_EX);
             $content = $phpCode;
         }
 
         //check the file content is the same as the generated content then return file location
-        if($currentCode == $content){
+        if ($currentCode == $content) {
             return $location;
-        }else{
+        } else {
             file_put_contents($location, $phpCode, LOCK_EX);
             return $location;
         }
@@ -291,34 +303,35 @@ class ui_templateparser {
      * @var deathAfter the number of seconds old a cache file can be deafult 7 days
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static function clearOldCache($deathAfter = '604800'){
+    static function clearOldCache($deathAfter = '604800')
+    {
         //dont check every time!
         $rollDice = rand(1, ui_templateparser::$deleteCacheChance);
 
         //1 in X chance of checking all files
-        if($rollDice == 1){
+        if ($rollDice == 1) {
             //get all files and folders in storage location
             $contents = glob(ui_templateparser::$storageLocation . "*");
-             
+
             //loop each file and folder
-            foreach($contents as $content){                
-                 //Is folder
-                 if(is_dir($content)){
-                      $cacheFilesArray = glob($content . "/*"); 
-                      foreach($cacheFilesArray as $cacheFile){
-                          if(!is_dir($cacheFile)){
-                                $time = time() - $deathAfter;
-                                if(filemtime($cacheFile) <= $time){
-                                    //Delete cache files
-                                    $path = pathinfo($cacheFile);
-                                    chdir($path['dirname']); 
-                                    unlink($path['filename'].'.cache');
-                                }
-                          }
-                      }
-                 }
+            foreach ($contents as $content) {
+                //Is folder
+                if (is_dir($content)) {
+                    $cacheFilesArray = glob($content . "/*");
+                    foreach ($cacheFilesArray as $cacheFile) {
+                        if (!is_dir($cacheFile)) {
+                            $time = time() - $deathAfter;
+                            if (filemtime($cacheFile) <= $time) {
+                                //Delete cache files
+                                $path = pathinfo($cacheFile);
+                                chdir($path['dirname']);
+                                unlink($path['filename'] . '.cache');
+                            }
+                        }
+                    }
+                }
             }
-            chdir('/'); 
+            chdir('/');
         }
     }
 
@@ -326,26 +339,27 @@ class ui_templateparser {
      * Set the root of the temp path location
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static public function setLocation(){
-        self::$storageLocation =  ctrl_options::GetSystemOption('zpanel_root').self::$storageLocation;
+    static public function setLocation()
+    {
+        self::$storageLocation = ctrl_options::GetSystemOption('zpanel_root') . self::$storageLocation;
     }
-
 
     /**
      * Run the php code and return it
      * @var code the php code
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static function runPHP($template_code){
+    static function runPHP($template_code)
+    {
         //load from DB or set a default
         $evalOrCache = ui_templateparser::$evalOrCache;
 
         //selected eval or file cache
-        if($evalOrCache == 1){
+        if ($evalOrCache == 1) {
             $fileLocation = ui_templateparser::CheckFileCache($template_code);
             return include($fileLocation);
-        }else{
-            return eval('?>' . $template_code); 
+        } else {
+            return eval('?>' . $template_code);
         }
     }
 
@@ -353,7 +367,8 @@ class ui_templateparser {
      * All br tags to be used using the zpanel br tag loader
      * @author Sam Mottley (smottley@zpanelcp.com)
      */
-    static function allowBr($templateCode){
+    static function allowBr($templateCode)
+    {
         return str_replace("ZP(br)", '<br/>', $templateCode);
     }
 
@@ -363,14 +378,14 @@ class ui_templateparser {
      * @param string $template_path The full path to the system template (or user template).
      * @return sting The processed template HTML.
      */
-    static function Generate($template_path) {
+    static function Generate($template_path)
+    {
         self::setLocation();
         $template_raw = file_get_contents($template_path . "/master.ztml");
         $template_code = ui_templateparser::allowBr(ui_templateparser::CompileFunctions($template_raw));
         ui_templateparser::clearOldCache();
         return ui_templateparser::runPHP($template_code);
     }
-
 
 }
 
