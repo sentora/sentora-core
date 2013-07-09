65a861b2f30c5f212534507c251b3b9e186df7b6
Kevin Andrews <kevin@nforced.co.uk>
2013-07-09 01:19:52 +0100

    Removed duplication of arguments when building command dependant on OS. Changed to use system class for security.

--- modules/webalizer_stats/hooks/OnDaemonHour.hook.php
+++ modules/webalizer_stats/hooks/OnDaemonHour.hook.php
@@ -24,50 +24,49 @@ function GenerateWebalizerStats()
         if ( !file_exists( ctrl_options::GetSystemOption( 'zpanel_root' ) . "modules/webalizer_stats/stats/" . $rowvhost[ 'ac_user_vc' ] . "/" . $rowvhost[ 'vh_name_vc' ] ) ) {
             @mkdir( ctrl_options::GetSystemOption( 'zpanel_root' ) . "modules/webalizer_stats/stats/" . $rowvhost[ 'ac_user_vc' ] . "/" . $rowvhost[ 'vh_name_vc' ], 0755, TRUE );
         }
+
+        /** set webalizer command dependant on OS */
         if ( sys_versions::ShowOSPlatformVersion() == "Windows" ) {
-            $runcommand =
-                ctrl_options::GetSystemOption( 'zpanel_root' ) .
-                "modules/webalizer_stats/bin/webalizer.exe -o " .
-                escapeshellarg( ctrl_options::GetSystemOption( 'zpanel_root' ) ) .
-                "modules/webalizer_stats/stats/" .
-                escapeshellarg( $rowvhost[ 'ac_user_vc' ] ) .
-                "/" . escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                " -d -F clf -n " . escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                "  " .
-                escapeshellarg( ctrl_options::GetSystemOption( 'log_dir' ) ) .
-                "domains/" .
-                escapeshellarg( $rowvhost[ 'ac_user_vc' ] ) .
-                "/" .
-                escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                "-access.log"
-            ;
+            $command = ctrl_options::GetSystemOption( 'zpanel_root' ) .
+                'modules/webalizer_stats/bin/webalizer.exe';
         }
         else {
             chmod( ctrl_options::GetSystemOption( 'zpanel_root' ) . "modules/webalizer_stats/bin/webalizer", 4777 );
-            $runcommand =
-                "(cd " .
-                escapeshellarg( ctrl_options::GetSystemOption( 'zpanel_root' ) ) .
-                "modules/webalizer_stats/bin" .
-                " && exec " .
-                "webalizer -o " .
-                escapeshellarg( ctrl_options::GetSystemOption( 'zpanel_root' ) ) .
-                "modules/webalizer_stats/stats/" .
-                escapeshellarg( $rowvhost[ 'ac_user_vc' ] ) .
-                "/" .
-                escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                " -d -F clf -n " .
-                escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                "  " .
-                escapeshellarg( ctrl_options::GetSystemOption( 'log_dir' ) ) .
-                "domains/" .
-                escapeshellarg( $rowvhost[ 'ac_user_vc' ] ) .
-                "/" .
-                escapeshellarg( $rowvhost[ 'vh_name_vc' ] ) .
-                "-access.log)"
-            ;
+            $command = "webalizer";
         }
+
+        /** all other args and flags are the same so keep them outsite to avoid duplication */
+        $flag        = '-o';
+        
+        $secondFlags = '-d -F clf -n';
+
+        $domain = $rowvhost[ 'vh_name_vc' ];
+
+        $logFile = realpath( ctrl_options::GetSystemOption( 'log_dir' ) .
+            'domains/' .
+            $rowvhost[ 'ac_user_vc' ] .
+            '/' .
+            $rowvhost[ 'vh_name_vc' ] .
+            '-access.log' );
+
+        $statsPath = ctrl_options::GetSystemOption( 'zpanel_root' ) .
+            "modules/webalizer_stats/stats/" .
+            $rowvhost[ 'ac_user_vc' ] .
+            '/' .
+            $rowvhost[ 'vh_name_vc' ];
+
+        /** build arg array, this is in the required order! do not just change the order of this array */
+        $args = array(
+            $logFile,
+            $flag,
+            $statsPath,
+            $secondFlags,
+            $domain,
+        );
+
         echo "Generating stats for: " . $rowvhost[ 'ac_user_vc' ] . "/" . $rowvhost[ 'vh_name_vc' ] . fs_filehandler::NewLine();
-        system( $runcommand );
+        $returnValue = ctrl_system::systemCommand( $command, $args );
+        echo (0 === $returnValue ? 'Succeeded' : 'Failed') . fs_filehandler::NewLine();
     }
 }
 
