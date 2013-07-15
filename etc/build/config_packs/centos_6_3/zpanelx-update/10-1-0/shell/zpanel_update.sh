53ff11888c11f14f56db7a2c52d5680d9081bf0f
Kevin Andrews <kevin@nforced.co.uk>
2013-07-15 23:48:28 +0100

    Fixes postfix mysql vulnerability on any outstanding installations.

--- etc/build/config_packs/centos_6_3/zpanelx-update/10-1-0/shell/zpanel_update.sh
+++ etc/build/config_packs/centos_6_3/zpanelx-update/10-1-0/shell/zpanel_update.sh
@@ -8,21 +8,22 @@ genpasswd() {
 }
 
 ## Determine if we need to update the postfix user
-result=`mysql -u postfix -ppostfx --skip-column-names -e "SHOW DATABASES LIKE 'zpanel_postfix'"`
+result=`mysql -u postfix -ppostfix --skip-column-names -e "SHOW DATABASES LIKE 'zpanel_postfix'"`
 
 if [ "$result" == "zpanel_postfix" ]; then
+	
 	password=`genpasswd`;
 	mysqlrootpass=`cat /root/mysqlrootpass`
 	echo "UPDATE mysql.user SET Password=PASSWORD('$password') WHERE User='postfix' AND Host='localhost';" | mysql -u root -p$mysqlrootpass
-	sed -i "s|password = postfix|myhostname = $password|" /etc/zpanel/configs/postfix/mysql-relay_domains_maps.cf
-	sed -i "s|password = postfix|myhostname = $password|" /etc/zpanel/configs/postfix/mysql-virtual_alias_maps.cf
-	sed -i "s|password = postfix|myhostname = $password|" /etc/zpanel/configs/postfix/mysql-virtual_domains_maps.cf
-	sed -i "s|password = postfix|myhostname = $password|" /etc/zpanel/configs/postfix/mysql-virtual_mailbox_limit_maps.cf
-	sed -i "s|password = postfix|myhostname = $password|" /etc/zpanel/configs/postfix/mysql-virtual_mailbox_maps.cf
+	echo "FLUSH PRIVILEGES;" | mysql -u root -p$mysqlrootpass
+	
+	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-relay_domains_maps.cf
+	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_alias_maps.cf
+	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_domains_maps.cf
+	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_mailbox_limit_maps.cf
+	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_mailbox_maps.cf
 	sed -i "s|\$db_password \= \'postfix\'\;|\$db_password \= \'$password\'\;|" /etc/zpanel/configs/postfix/vacation.conf
-	sed -i "s|connect = host=localhost dbname=zpanel_postfix user=postfix password=postfix|connect = host=localhost dbname=zpanel_postfix user=postfix password=$password" /etc/zpanel/configs/dovecot/dovecot-dict-quota.conf
-	sed -i "s|connect = host=localhost dbname=zpanel_postfix user=postfix password=postfix|connect = host=localhost dbname=zpanel_postfix user=postfix password=$password" /etc/zpanel/configs/dovecot/dovecot-mysql.conf
+	sed -i "s|connect \= host\=localhost dbname\=zpanel_postfix user\=postfix password\=postfix|connect \= host=localhost dbname\=zpanel_postfix user\=postfix password\=$password" /etc/zpanel/configs/dovecot/dovecot-dict-quota.conf
+	sed -i "s|connect \= host\=localhost dbname\=zpanel_postfix user\=postfix password\=postfix|connect \= host=localhost dbname\=zpanel_postfix user\=postfix password\=$password" /etc/zpanel/configs/dovecot/dovecot-mysql.conf
 	echo -e "Your new MySQL 'postfix' is : $password";
 fi
-
-
