c2d9435b7b23490ed53a4e46b62300930a59d693
Bobby Allen <bobbyallen.uk@gmail.com>
2013-10-28 13:50:54 +0000

    Adding removal of getdownload.php to ensure that if users who upgrade from 10.0.2 to 10.1.0 and didn't follow our manual patch advice this is now automatically taken care of during the upgrade.

--- etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-0/shell/zpanel_update.sh
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-0/shell/zpanel_update.sh
@@ -11,12 +11,12 @@ genpasswd() {
 result=`mysql -u postfix -ppostfix --skip-column-names -e "SHOW DATABASES LIKE 'zpanel_postfix'"`
 
 if [ "$result" == "zpanel_postfix" ]; then
-	
+
 	password=`genpasswd`;
 	mysqlrootpass=`cat /root/mysqlrootpass`
 	echo "UPDATE mysql.user SET Password=PASSWORD('$password') WHERE User='postfix' AND Host='localhost';" | mysql -u root -p$mysqlrootpass
 	echo "FLUSH PRIVILEGES;" | mysql -u root -p$mysqlrootpass
-	
+
 	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-relay_domains_maps.cf
 	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_alias_maps.cf
 	sed -i "s|password \= postfix|password \= $password|" /etc/zpanel/configs/postfix/mysql-virtual_domains_maps.cf
@@ -28,3 +28,5 @@ if [ "$result" == "zpanel_postfix" ]; then
 	echo -e "Your new MySQL 'postfix' is : $password";
 	echo -e "MySQL Postfix Password : $password" >> /root/passwords.txt
 fi
+
+rm /etc/zpanel/panel/modules/backupmgr/code/getdownload.php
