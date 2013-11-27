de1e87a5bb2f633d4ce4a92345006cae7651a946
Bobby Allen <bobbyallen.uk@gmail.com>
2013-11-27 14:22:49 +0000

    Implements UTF-8 support for PDO, closes feature request #667 (http://bugs.zpanelcp.com/view.php?id=667)

--- inc/dbc.inc.php
+++ inc/dbc.inc.php
@@ -22,7 +22,7 @@ global $zdbh;
 $zlo = new debug_logger();
 
 try {
-    $zdbh = new db_driver("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $zdbh = new db_driver("mysql:host=$host;dbname=$dbname", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     $zdbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     $zlo->method = "text";
