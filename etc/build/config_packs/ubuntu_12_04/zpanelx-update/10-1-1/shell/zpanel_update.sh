770eab666fef6448d4435c005546a685fb239d4c
Kimpe Andy <andykimpe@gmail.com>
2013-12-31 06:15:27 +0100

    Update zpanel_update.sh

--- etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-1/shell/zpanel_update.sh
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-1/shell/zpanel_update.sh
@@ -1,6 +1,8 @@
 # Security enhancement for MySQL.
 sed -i "/ssl-key=/a \secure-file-priv = /var/tmp" /etc/mysql/my.cnf
 
+if ! grep -q "apache ALL=NOPASSWD: /etc/zpanel/panel/bin/zsudo" /etc/sudoers; then sed -i "s|apache ALL=NOPASSWD: /etc/zpanel/panel/bin/zsudo|www-data ALL=NOPASSWD: /etc/zpanel/panel/bin/zsudo|"  ; fi
+
 # Double check fixing permissions for CRON jobs.
 chmod -R 644 /var/spool/cron/
 chmod -R 644 /etc/cron.d/
