94315ee5b3aec51f7ddf59a0729d2a2ec84d6edf
Bobby Allen <ballen@bobbyallen.me>
2014-02-03 19:09:56 +0000

    Decided to force delete 'getdownload.php' in the upgrade scripts as a safety precaution for those users that may not have patched their installations as per our security bulletins.

--- etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-1/shell/zpanel_update.sh
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-update/10-1-1/shell/zpanel_update.sh
@@ -9,3 +9,6 @@ chmod -R 644 /etc/cron.d/
 
 # Removal of Password Directories module
 rm -Rf /etc/zpanel/panel/modules/htpasswd
+
+# Force removal of the 'getdownload.php' file for those that failed to delete it as per our security bulletin.
+rm -f /etc/zpanel/panel/modules/backupmgr/code/getdownload.php
