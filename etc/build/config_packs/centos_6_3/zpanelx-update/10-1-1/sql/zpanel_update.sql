71ababa59191c27e8deb5364df97f8646d423ab8
jacobg830 <jacobg830@icloud.com>
2014-02-05 22:18:58 +0000

    Added the fixed HTML entities to SQL Upgrades

--- etc/build/config_packs/centos_6_3/zpanelx-update/10-1-1/sql/zpanel_update.sql
+++ etc/build/config_packs/centos_6_3/zpanelx-update/10-1-1/sql/zpanel_update.sql
@@ -1,4 +1,4 @@
-/* Update SQL for Ubuntu 12.04 ZPanel 10.1.0 to 10.1.1 */
+/* Update SQL for CentOS 6.3 ZPanel 10.1.0 to 10.1.1 */
 USE `zpanel_core`;
 
 /* VERSION SPECIFIC UPDATE SQL STATEMENTS */
@@ -9,3 +9,10 @@ UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  '10.1.1' WHERE  `so_nam
 
 /* Removal of Password Protect directories module */
 DELETE FROM `zpanel_core`.`x_modules` WHERE `x_modules`.`mo_name_vc` = 'Protect Directories';
+
+/* Update module entries and their translations */
+UPDATE  `zpanel_core`.`x_modules` SET  `mo_desc_tx` = 'The backup manager module enables you to backup your entire hosting account including all your MySQL&reg; databases.' WHERE  `mo_id_pk` = '12';
+UPDATE  `zpanel_core`.`x_modules` SET  `mo_desc_tx` = 'MySQL&reg; databases are used by many PHP applications such as forums and ecommerce systems, below you can manage and create MySQL&reg; databases.' WHERE  `mo_id_pk` = '24';
+UPDATE  `zpanel_core`.`x_modules` SET  `mo_desc_tx` = 'MySQL&reg; Users allows you to add users and permissions to your MySQL&reg; databases.' WHERE  `mo_id_pk` = '39';
+UPDATE  `zpanel_core`.`x_translations` SET  `tr_en_tx` = 'The backup manager module enables you to backup your entire hosting account including all your MySQL&reg; databases.' WHERE  `tr_id_pk` = '103';
+UPDATE  `zpanel_core`.`x_translations` SET  `tr_de_tx` = 'Der Backup-Manager-Modul ermöglicht es Ihnen, Ihre gesamte Hosting-Account inklusive aller Ihrer MySQL &reg; Datenbank-Backup.' WHERE  `tr_id_pk` = '103';
