5dae22252f9368a87c57ad83b133d9f295162b87
Bobby Allen <bobbyallen.uk@gmail.com>
2014-02-27 11:38:15 +0000

    SQL installation script and upgrade scripts updates.

--- etc/build/config_packs/ms_windows/zpanelx-update/10-1-1/zpanel_update.sql
+++ etc/build/config_packs/ms_windows/zpanelx-update/10-1-1/zpanel_update.sql
@@ -17,6 +17,32 @@ UPDATE  `zpanel_core`.`x_modules` SET  `mo_desc_tx` = 'MySQL&reg; Users allows y
 UPDATE  `zpanel_core`.`x_translations` SET  `tr_en_tx` = 'The backup manager module enables you to backup your entire hosting account including all your MySQL&reg; databases.' WHERE  `tr_id_pk` = '103';
 UPDATE  `zpanel_core`.`x_translations` SET  `tr_de_tx` = 'Der Backup-Manager-Modul ermöglicht es Ihnen, Ihre gesamte Hosting-Account inklusive aller Ihrer MySQL &reg; Datenbank-Backup.' WHERE  `tr_id_pk` = '103';
 
+/* Table structure for table `x_dns_create` */
+CREATE TABLE IF NOT EXISTS `zpanel_core`.`x_dns_create` (
+  `dc_id_pk` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `dc_acc_fk` int(6) DEFAULT NULL,
+  `dc_type_vc` varchar(50) DEFAULT NULL,
+  `dc_host_vc` varchar(100) DEFAULT NULL,
+  `dc_ttl_in` int(30) DEFAULT NULL,
+  `dc_target_vc` varchar(100) DEFAULT NULL,
+  `dc_priority_in` int(50) DEFAULT NULL,
+  `dc_weight_in` int(50) DEFAULT NULL,
+  `dc_port_in` int(50) DEFAULT NULL,
+  PRIMARY KEY (`dc_id_pk`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+/* Data for the table `x_dns_create` */
+INSERT INTO `zpanel_core`.`x_dns_create` (`dc_id_pk`, `dc_acc_fk`, `dc_type_vc`, `dc_host_vc`, `dc_ttl_in`, `dc_target_vc`, `dc_priority_in`, `dc_weight_in`, `dc_port_in`) VALUES
+(1, 0, 'A', '@', 3600, ':IP:', NULL, NULL, NULL),
+(2, 0, 'CNAME', 'www', 3600, '@', NULL, NULL, NULL),
+(3, 0, 'CNAME', 'ftp', 3600, '@', NULL, NULL, NULL),
+(4, 0, 'A', 'mail', 86400, ':IP:', NULL, NULL, NULL),
+(5, 0, 'MX', '@', 86400, 'mail.:DOMAIN:', 10, NULL, NULL),
+(6, 0, 'A', 'ns1', 172800, ':IP:', NULL, NULL, NULL),
+(7, 0, 'A', 'ns2', 172800, ':IP:', NULL, NULL, NULL),
+(8, 0, 'NS', '@', 172800, 'ns1.:DOMAIN:', NULL, NULL, NULL),
+(9, 0, 'NS', '@', 172800, 'ns2.:DOMAIN:', NULL, NULL, NULL);
+
 /* Update the default zpanel debug mode from 'dev' to 'prod' */
 UPDATE  `zpanel_core`.`x_settings` SET  `so_value_tx` =  'prod' WHERE  `so_name_vc` = 'debug_mode';
 
--- etc/build/config_packs/ubuntu_11_10/zpanel_core.sql
+++ etc/build/config_packs/ubuntu_11_10/zpanel_core.sql
@@ -186,6 +186,32 @@ CREATE TABLE `x_dns` (
 
 /*Data for the table `x_dns` */
 
+/* Table structure for table `x_dns_create` */
+CREATE TABLE IF NOT EXISTS `zpanel_core`.`x_dns_create` (
+  `dc_id_pk` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `dc_acc_fk` int(6) DEFAULT NULL,
+  `dc_type_vc` varchar(50) DEFAULT NULL,
+  `dc_host_vc` varchar(100) DEFAULT NULL,
+  `dc_ttl_in` int(30) DEFAULT NULL,
+  `dc_target_vc` varchar(100) DEFAULT NULL,
+  `dc_priority_in` int(50) DEFAULT NULL,
+  `dc_weight_in` int(50) DEFAULT NULL,
+  `dc_port_in` int(50) DEFAULT NULL,
+  PRIMARY KEY (`dc_id_pk`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+/* Data for the table `x_dns_create` */
+INSERT INTO `zpanel_core`.`x_dns_create` (`dc_id_pk`, `dc_acc_fk`, `dc_type_vc`, `dc_host_vc`, `dc_ttl_in`, `dc_target_vc`, `dc_priority_in`, `dc_weight_in`, `dc_port_in`) VALUES
+(1, 0, 'A', '@', 3600, ':IP:', NULL, NULL, NULL),
+(2, 0, 'CNAME', 'www', 3600, '@', NULL, NULL, NULL),
+(3, 0, 'CNAME', 'ftp', 3600, '@', NULL, NULL, NULL),
+(4, 0, 'A', 'mail', 86400, ':IP:', NULL, NULL, NULL),
+(5, 0, 'MX', '@', 86400, 'mail.:DOMAIN:', 10, NULL, NULL),
+(6, 0, 'A', 'ns1', 172800, ':IP:', NULL, NULL, NULL),
+(7, 0, 'A', 'ns2', 172800, ':IP:', NULL, NULL, NULL),
+(8, 0, 'NS', '@', 172800, 'ns1.:DOMAIN:', NULL, NULL, NULL),
+(9, 0, 'NS', '@', 172800, 'ns2.:DOMAIN:', NULL, NULL, NULL);
+
 /*Table structure for table `x_faqs` */
 
 DROP TABLE IF EXISTS `x_faqs`;
