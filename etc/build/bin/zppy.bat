25756cb793ce42a24205c9038b70db4306a5ac6d
jacobg830 <jacobg830@icloud.com>
2014-07-02 16:37:33 +0100

    Deleted Windows build and config files

--- etc/build/bin/zppy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Provides a command line pass through for the ZPanel 'zppy' client.
-REM This file should be placed in the Windows\System folder to enable easy use of the 'zppy' command.
-php C:\zpanel\panel\bin\zppy %1 %2 %3
