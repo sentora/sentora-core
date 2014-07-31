3c3f6f575c5ea7ff7b32dcce67f30dd9126398c5
jacobg830 <jacobg830@icloud.com>
2014-07-31 11:22:54 +0100

    NetBeans auto format

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -38,7 +38,7 @@ if (isset($_POST['inForgotPassword'])) {
     runtime_csfr::Protect();
     $randomkey = runtime_randomstring::randomHash();
     $forgotPass = runtime_xss::xssClean($_POST['inForgotPassword']);
-    $sth = $zdbh->prepare("SELECT ac_id_pk, ac_user_vc, ac_email_vc  FROM x_accounts WHERE ac_email_vc = :forgotPass AND ac_deleted_ts IS NULL");
+    $sth = $zdbh->prepare("SELECT ac_id_pk, ac_user_vc, ac_email_vc FROM x_accounts WHERE ac_email_vc = :forgotPass AND ac_deleted_ts IS NULL");
     $sth->bindParam(':forgotPass', $forgotPass);
     $sth->execute();
     $rows = $sth->fetchAll();
@@ -119,17 +119,17 @@ if (isset($_POST['inUsername'])) {
 }
 
 if (isset($_COOKIE['zUser'])) {
-    
+
     if (isset($_COOKIE['zSec'])) {
-        if($_COOKIE['zSec'] == false) {
+        if ($_COOKIE['zSec'] == false) {
             $secure = false;
         } else {
             $secure = true;
         }
-    }else{
+    } else {
         $secure = true;
     }
-    
+
     ctrl_auth::Authenticate($_COOKIE['zUser'], $_COOKIE['zPass'], false, true, $secure);
 }
 
