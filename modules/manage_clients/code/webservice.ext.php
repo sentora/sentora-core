e9d1f81aa18cf1250da4664b66ffadaa6993db91
Bobby Allen <bobbyallen.uk@gmail.com>
2015-01-22 21:07:54 +0000

    Fixes typo in variable name.

--- modules/manage_clients/code/webservice.ext.php
+++ modules/manage_clients/code/webservice.ext.php
@@ -92,7 +92,7 @@ class webservice extends ws_xmws {
         }
         $dataobject = new runtime_dataobject();
         $dataobject->addItemValue('response', '');
-        $dataobject->addItemValue('content', $respons);
+        $dataobject->addItemValue('content', $response);
         return $dataobject->getDataObject();
     }
 
