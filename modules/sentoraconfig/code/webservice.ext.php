e135215b3e00fbc9704d586f929f3867eb07ba6d
Nigel Caldwell <p10god.nz@gmail.com>
2022-04-05 11:00:00 +1200

    Starting to Standardize Modules
    Updated module version numbers, based off Github Commits after the Name change from ZPanelX to Sentora.
    
    Added XML & ZPP for easy updates.

--- modules/sentoraconfig/code/webservice.ext.php
+++ modules/sentoraconfig/code/webservice.ext.php
@@ -1,12 +1,28 @@
 <?php
 
 /**
- * @package zpanelx
- * @subpackage modules
- * @author Bobby Allen (ballen@bobbyallen.me)
- * @copyright ZPanel Project (http://www.zpanelcp.com/)
- * @link http://www.zpanelcp.com/
- * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Sentora - A Cross-Platform Open-Source Web Hosting Control panel.
+ *
+ * @package ZPanel
+ * @version $Id$
+ * @author Bobby Allen - ballen@bobbyallen.me
+ * @copyright (c) 2008-2014 ZPanel Group - http://www.zpanelcp.com/
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License v3
+ *
+ * This program (Sentora) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 class webservice extends ws_xmws {
 
