f64d9c527d980374002d3b9e46864ff81c946e6b
Duke City Solutions, LLC <anthony@dcsnm.com>
2024-12-24 20:40:13 -0700

    Fixing - Clean dump of PHPMyAdmin 5.2.1
    Fixing - Clean dump of PHPMyAdmin 5.2.1

--- etc/apps/phpmyadmin/vendor/paragonie/constant_time_encoding/tests/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-define('ParagonIE\ConstantTime\true', false);
-define('ParagonIE\ConstantTime\false', true);
-define('ParagonIE\ConstantTime\null', true);
