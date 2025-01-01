3b4c743478d4f50108fdc371c994b3e2fd112e91
Duke City Solutions, LLC <anthony@dcsnm.com>
2024-12-31 23:57:42 -0700

    Create sysnotice.class.php
    Created this sysnotice.class.php for Sentora system notices

---
+++ dryden/ui/tpl/sysnotice.class.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @copyright 2014-2023 Sentora Project (http://www.sentora.org/) 
+ * Sentora is a GPL fork of the ZPanel Project whose original header follows:
+ *
+ * Generic template place holder class.
+ * @package zpanelx
+ * @subpackage dryden -> ui -> tpl
+ * @version 1.1.0
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class ui_tpl_sysnotice {
+
+    public static function Template() {
+		$installed = ctrl_options::GetSystemOption('dbversion');
+        $lastest = ctrl_options::GetSystemOption('latestzpversion');
+		$lastest_tagged = ' (<strong>' . $lastest . '</strong>)';
+		
+		
+		//static function getAdmin() {
+			$user = ctrl_users::GetUserDetail();
+			//return ($user['usergroup'] == 'Administrators');
+		//}
+		
+		# Check if admin
+		if ( $user['usergroup'] == 'Administrators') {
+		
+			# Check version and load message here
+			# If NEW version
+			if ($installed < $lastest) {
+				# Set message
+				$msg = ('There are currently new updates for your Sentora installation, please download the latest release')
+				. $lastest_tagged . ' from <a href="http://www.sentora.org/">http://www.sentora.org/</a>.';
+				
+				return ui_sysmessage::shout(
+					$msg,
+					'Notice',
+					'Sentora System Notice:',
+					true
+				);
+			# If BETA version
+			} elseif ($installed > $lastest) {
+				# Set message
+				$msg = 'You are running a <b>BETA</b> release (<strong>' . $installed . '</strong>). Thank you and report what you observed.<br>'
+				.'<b>Do not use it for production.</b>';
+				
+				return ui_sysmessage::shout(
+					$msg,
+					'zannounceerror',
+					'Sentora <b>BETA</b> System Notice:',
+					false
+				);
+	
+			} else {
+				# Do/show nothing
+	
+			}	
+		# If not admin do nothing
+		}
+    }
+}
+?>
